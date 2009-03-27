71bb08c1ed39f5646d6259ef82dd40be41fc8d95
Daniel Krueger <daniel.krueger@systec-electronic.com>
2009-03-27 15:42:40 +0000

    Changed Makefile of MN demo for ECUcore-5484
    Removed dummy C files that just contain includes of real C files.
    So this directory looks much cleaner.
    Disadvantage: generated .o files are spawned over whole source tree.
    There may be collisions with other projects working with the same files.

--- Examples/PLCcore-CF54/Linux/gnu/demo_mn_kernel/EplSdoUdpu.c
+++
@@ -1,7 +0,0 @@
-
-// The Linux kernel build system for drivers/modules requires that all
-// source files are located in the same directory as the makefile. To
-// fulfill this condition a wrapper file for each necessary source file
-// is located in this directory.
-
-#include "../../../../../EplStack/EplSdoUdpu.c"
--- Examples/PLCcore-CF54/Linux/gnu/demo_mn_kernel/fec.c
+++
@@ -1,9 +0,0 @@
-
-// The Linux kernel build system for drivers/modules requires that all
-// source files are located in the same directory as the makefile. To
-// fulfill this condition a wrapper file for each necessary source file
-// is located in this directory.
-
-#include "../../../../../Edrv/fec_548x/net/fec.c"
-
-
