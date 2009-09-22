d617c42f7b9b3770d4d7426642d399a063818c34
Daniel Krueger <daniel.krueger@systec-electronic.com>
2009-09-22 15:53:28 +0000

    Reverted FPGA design for EBV DBC3C40 board back to full 50 MHz
    but decreased I-Cache to 16 kB

--- Examples/altera_nios2/EBV_DBC3C40/design_nios2_openmac/EPL/DBC3C40_SRAM_hw.tcl
+++ Examples/altera_nios2/EBV_DBC3C40/design_nios2_openmac/EPL/DBC3C40_SRAM_hw.tcl
@@ -4,19 +4,19 @@
 
 
 # +-----------------------------------
-# |
+# | 
 # | DBC3C40_SRAM "IS61WV51216" v1.0
 # | devboards GmbH 2009.06.03.07:07:52
 # | 16bit SRAM Interface
-# |
-# |
-# |
+# | 
+# | 
+# | 
 # +-----------------------------------
 
 
 # +-----------------------------------
 # | module DBC3C40_SRAM
-# |
+# | 
 set_module_property DESCRIPTION "16bit SRAM Interface"
 set_module_property NAME DBC3C40_SRAM
 set_module_property VERSION 1.0
@@ -28,24 +28,24 @@ set_module_property DISPLAY_NAME IS61WV51216
 set_module_property TOP_LEVEL_HDL_FILE ""
 set_module_property INSTANTIATE_IN_SYSTEM_MODULE false
 set_module_property EDITABLE false
-# |
+# | 
 # +-----------------------------------
 
 # +-----------------------------------
 # | files
-# |
-# |
+# | 
+# | 
 # +-----------------------------------
 
 # +-----------------------------------
 # | parameters
-# |
-# |
+# | 
+# | 
 # +-----------------------------------
 
 # +-----------------------------------
 # | connection point avalon_tristate_slave
-# |
+# | 
 add_interface avalon_tristate_slave avalon_tristate end
 set_interface_property avalon_tristate_slave activeCSThroughReadLatency false
 set_interface_property avalon_tristate_slave bridgesToMaster ""
@@ -55,7 +55,7 @@ set_interface_property avalon_tristate_slave isNonVolatileStorage false
 set_interface_property avalon_tristate_slave maximumPendingReadTransactions 0
 set_interface_property avalon_tristate_slave printableDevice false
 set_interface_property avalon_tristate_slave readLatency 0
-set_interface_property avalon_tristate_slave readWaitTime 2
+set_interface_property avalon_tristate_slave readWaitTime 1
 set_interface_property avalon_tristate_slave setupTime 0
 set_interface_property avalon_tristate_slave timingUnits Cycles
 set_interface_property avalon_tristate_slave writeWaitStates 1
@@ -69,5 +69,5 @@ add_interface_port avalon_tristate_slave ncs chipselect_n Input 1
 add_interface_port avalon_tristate_slave wrn write_n Input 1
 add_interface_port avalon_tristate_slave rdn read_n Input 1
 add_interface_port avalon_tristate_slave ben byteenable_n Input 2
-# |
+# | 
 # +-----------------------------------
