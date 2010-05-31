10beb6eb683a52f28897c10e90ff5798247b8601
Michael Ulbricht <michael.ulbricht@systec-electronic.com>
2010-05-31 10:51:51 +0000

    EplDlluCal: Enclosed declaration of EplDlluCalIssueSyncRequest in EPL_DLL_PRES_CHAINING_MN check

--- Include/user/EplDlluCal.h
+++ Include/user/EplDlluCal.h
@@ -120,7 +120,10 @@ tEplKernel EplDlluCalDeleteNode(tEplDllNodeOpParam* pNodeOpParam_p);
 
 tEplKernel EplDlluCalIssueRequest(tEplDllReqServiceId Service_p, unsigned int uiNodeId_p, BYTE bSoaFlag1_p);
 
+#if EPL_DLL_PRES_CHAINING_MN != FALSE
 tEplKernel EplDlluCalIssueSyncRequest(tEplDllSyncRequest* pSyncRequest_p, unsigned int uiSize_p);
+#endif
+
 #endif
 
 
