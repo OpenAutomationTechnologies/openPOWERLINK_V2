33ad056dcbc9102ceb5c990a0048ce87835d9999
Michael Ulbricht <michael.ulbricht@systec-electronic.com>
2010-06-11 12:58:47 +0000

    demo_mn_82573_kernel: Increased number of SDO channels

--- Examples/X86/Linux/gnu/demo_mn_82573_kernel/EplCfg.h
+++ Examples/X86/Linux/gnu/demo_mn_82573_kernel/EplCfg.h
@@ -198,6 +198,17 @@
 //#define EPL_TIMER_USE_HIGHRES              FALSE
 
 
+// =========================================================================
+// SDO module specific defines
+// =========================================================================
+
+// increase the number of SDO channels, because we are master
+#define EPL_SDO_MAX_CONNECTION_ASND 100
+#define EPL_MAX_SDO_SEQ_CON         100
+#define EPL_MAX_SDO_COM_CON         100
+#define EPL_SDO_MAX_CONNECTION_UDP  50
+
+
 #endif //_EPLCFG_H_
 
 
