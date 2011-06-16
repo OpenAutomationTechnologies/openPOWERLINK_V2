4b05ac176ee30b06e83dbbc127567dae0a804773
Wolfgang Seiss <wolfgang.seiss@br-automation.com>
2011-06-16 12:20:32 +0200

    Joined Windows and Linux version of powerlink_user_lib

--- Examples/X86/Generic/powerlink_user_lib/EplCfg.h
+++ Examples/X86/Generic/powerlink_user_lib/EplCfg.h
@@ -71,8 +71,14 @@
 #ifndef _EPLCFG_H_
 #define _EPLCFG_H_
 
+#ifdef __linux__
+/* assure that system priorities of hrtimer and net-rx kernel threads are set appropriate */
+#define EPL_THREAD_PRIORITY_HIGH     75
+#define EPL_THREAD_PRIORITY_MEDIUM   50
+#define EPL_THREAD_PRIORITY_LOW      49
 
-
+#undef FTRACE_DEBUG
+#endif // __linux__
 
 // =========================================================================
 // generic defines which for whole EPL Stack
@@ -100,19 +106,53 @@
 #define BENCHMARK_MODULES       0 //0xEE800042L
 #endif
 
-// Default defug level:
+// Default debug level:
 // Only debug traces of these modules will be compiled which flags are set in define DEF_DEBUG_LVL.
 #ifndef DEF_DEBUG_LVL
-#define DEF_DEBUG_LVL           0xEC000084L
+//#define DEF_DEBUG_LVL           0xEC000084L
+#define DEF_DEBUG_LVL           (0xC00000000L)
 #endif
-//   EPL_DBGLVL_OBD         =   0x00000004L
-// * EPL_DBGLVL_ASSERT      =   0x20000000L
-// * EPL_DBGLVL_ERROR       =   0x40000000L
-// * EPL_DBGLVL_ALWAYS      =   0x80000000L
+
+//#define EPL_DBGLVL_EDRV                 DEBUG_LVL_01        // 0x00000001L
+//#define EPL_DBGLVL_DLL                  DEBUG_LVL_02        // 0x00000002L
+//#define EPL_DBGLVL_OBD                  DEBUG_LVL_03        // 0x00000004L
+//#define EPL_DBGLVL_NMTK                 DEBUG_LVL_04        // 0x00000008L
+//#define EPL_DBGLVL_NMTCN                DEBUG_LVL_05        // 0x00000010L
+//#define EPL_DBGLVL_NMTU                 DEBUG_LVL_06        // 0x00000020L
+//#define EPL_DBGLVL_NMTMN                DEBUG_LVL_07        // 0x00000040L
+//#define EPL_DBGLVL_CFM                  DEBUG_LVL_08        // 0x00000080L
+//#define EPL_DBGLVL_TIMERU               DEBUG_LVL_09        // 0x00000100L
+//#define EPL_DBGLVL_TIMERH               DEBUG_LVL_10        // 0x00000200L
+//#define EPL_DBGLVL_SDO                  DEBUG_LVL_25        // 0x01000000L
+//#define EPL_DBGLVL_VETH                 DEBUG_LVL_26        // 0x02000000L
+//#define EPL_DBGLVL_EVENTK               DEBUG_LVL_27        // 0x04000000L
+//#define EPL_DBGLVL_EVENTU               DEBUG_LVL_28        // 0x08000000L
+//#define EPL_DBGLVL_SHB                  DEBUG_LVL_29        // 0x10000000L
+//#define EPL_DBGLVL_ASSERT               DEBUG_LVL_ASSERT    // 0x20000000L
+//#define EPL_DBGLVL_ERROR                DEBUG_LVL_ERROR     // 0x40000000L
+//#define EPL_DBGLVL_ALWAYS               DEBUG_LVL_ALWAYS    // 0x80000000L
 
 
 // EPL_MODULE_INTEGRATION defines all modules which are included in
 // EPL application. Please add or delete modules for your application.
+#if defined __linux__
+#define EPL_MODULE_INTEGRATION (EPL_MODULE_OBDK \
+                               | EPL_MODULE_PDOK \
+                               | EPL_MODULE_PDOU \
+                               | EPL_MODULE_SDOS \
+                               | EPL_MODULE_SDOC \
+                               | EPL_MODULE_SDO_ASND \
+                               | EPL_MODULE_SDO_UDP \
+                               | EPL_MODULE_NMT_CN \
+                               | EPL_MODULE_NMTU \
+                               | EPL_MODULE_NMTK \
+                               | EPL_MODULE_DLLK \
+                               | EPL_MODULE_DLLU \
+                               | EPL_MODULE_CFM \
+                               | EPL_MODULE_NMT_MN)
+//                               | EPL_MODULE_VETH
+//                               | EPL_MODULE_OBDU
+#else if defined WIN32
 #define EPL_MODULE_INTEGRATION (EPL_MODULE_OBDK \
                                | EPL_MODULE_PDOK \
                                | EPL_MODULE_PDOU \
@@ -128,6 +168,7 @@
                                | EPL_MODULE_NMT_MN)
 //                               | EPL_MODULE_VETH
 //                               | EPL_MODULE_OBDU
+#endif
 
 // =========================================================================
 // EPL ethernet driver (Edrv) specific defines
@@ -153,6 +194,7 @@
 // number of used ethernet controller
 //#define EDRV_USED_ETH_CTRL              1
 
+#define EDRV_USE_DIAGNOSTICS            TRUE
 
 // =========================================================================
 // Data Link Layer (DLL) specific defines
@@ -203,8 +245,8 @@
 // =========================================================================
 
 // if TRUE the high resolution timer module will be used
-#define EPL_TIMER_USE_HIGHRES               TRUE
-//#define EPL_TIMER_USE_HIGHRES               FALSE
+#define EPL_TIMER_USE_HIGHRES              TRUE
+//#define EPL_TIMER_USE_HIGHRES              FALSE
 
 
 #define EPL_CFM_CONFIGURE_CYCLE_LENGTH      TRUE
@@ -221,6 +263,3 @@
 
 
 #endif //_EPLCFG_H_
-
-
-
