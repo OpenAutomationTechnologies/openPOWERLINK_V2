cda430bc11882c2a80074570956e0bd6f761e57c
Wolfgang Wallner <wolfgang.wallner@br-automation.com>
2011-09-15 09:43:55 +0200

    Generic_Demos: Add EplTgtConio for cross-platform implementations of kbhit, getch, etc.

---
+++ Include/EplTgtConio.h
@@ -0,0 +1,75 @@
+/*******************************************************************************
+
+  (c) Bernecker + Rainer Ges.m.b.H.,  B&R Strasse 1, 5142 Eggelsberg, Austria
+      www.br-automation.com
+
+  Description:  Library to provide console IO primitives
+                (kbhit, getch, etc.) on different plattforms.
+
+  License:
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    3. Neither the name of Bernecker + Rainer Ges.m.b.H nor the names of its
+       contributors may be used to endorse or promote products derived
+       from this software without prior written permission. For written
+       permission, please contact office@br-automation.com.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+
+    Severability Clause:
+
+        If a provision of this License is or becomes illegal, invalid or
+        unenforceable in any jurisdiction, that shall not affect:
+        1. the validity or enforceability in that jurisdiction of any other
+           provision of this License; or
+        2. the validity or enforceability in other jurisdictions of that or
+           any other provision of this License.
+
+*******************************************************************************/
+
+#ifndef _EPL_TGT_CONIO_H_
+#define _EPL_TGT_CONIO_H_
+
+//=========================================================================//
+// Includes                                                                //
+//=========================================================================//
+//#include <stdint.h>
+
+//=========================================================================//
+// Constants                                                               //
+//=========================================================================//
+
+//=========================================================================//
+// Type definitions                                                        //
+//=========================================================================//
+
+//=========================================================================//
+// Function prototypes                                                     //
+//=========================================================================//
+
+int     EplTgtGetch( void );
+int     EplTgtKbhit( void );
+void    EplTgtMilliSleep( unsigned int MilliSeconds );
+
+#endif  // _EPL_TGT_CONIO_H_
