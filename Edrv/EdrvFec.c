477b31342975c42cb8cf660c95ff63a2399ff482
Michael Ulbricht <michael.ulbricht@systec-electronic.com>
2011-11-07 15:22:33 +0100

    EdrvFec: Add SYS TEC copyright and GPL header

--- Edrv/EdrvFec.c
+++ Edrv/EdrvFec.c
@@ -1,4 +1,26 @@
 /*
+ * Fast Ethernet Controller (FEC) for Freescale i.MX51
+ * Copyright (c) 2011 SYS TEC electronic GmbH
+ * www.systec-electronic.com
+ *
+ * Derived from Linux kernel 2.6.35.3-433-g0fae922 /drivers/net/fec.c
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *
+ * Original Header:
  * Fast Ethernet Controller (FEC) driver for Motorola MPC8xx.
  * Copyright (c) 1997 Dan Malek (dmalek@jlc.net)
  *
@@ -20,6 +42,7 @@
  *
  * Support for FEC IEEE 1588.
  * Copyright (C) 2010 Freescale Semiconductor, Inc.
+ *
  */
 
 
