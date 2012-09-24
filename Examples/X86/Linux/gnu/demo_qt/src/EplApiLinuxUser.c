56f5ce6a6b68b3fba36338334dcbb777aaea72b2
Josef Baumgartner <josef.baumgartner@br-automation.com>
2012-09-24 13:18:11 +0200

    [TASK] Remove Linux demo_qt application
    Remove demo because the generic process image based demo could be used
    instead. Also remove Qt_MN object dictionary because it is only used by
    demo_qt.

--- Examples/X86/Linux/gnu/demo_qt/src/EplApiLinuxUser.c
+++
@@ -1,7 +0,0 @@
-
-// The Qt project generation (qmake -project) requires that all
-// source files are located below the applications root directory.
-
-#include "../../../../../../EplStack/EplApiLinuxUser.c"
-
-
