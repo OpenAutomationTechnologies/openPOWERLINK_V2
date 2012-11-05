8d29334d84c9377b8f92f1c6407111d0223311ea
Josef Baumgartner <josef.baumgartner@br-automation.com>
2012-11-05 10:36:21 +0100

    [TASK] Add bit-sized integer type macros
    Change-Id: Ia08741225e7544475a37f8b5d343ba6b6d605124
    Reviewed-on: http://brateggevoat1/gerrit/374
    Reviewed-by: Josef Baumgartner <josef.baumgartner@br-automation.com>
    Tested-by: Jenkins <jenkins@brateggevoat1.br-automation.co.at>

--- Include/global.h
+++ Include/global.h
@@ -529,6 +529,39 @@
         #define BOOL unsigned char
     #endif
 
+    // --- bit sized types ---
+    #ifndef UINT8
+        #define UINT8 unsigned char
+    #endif
+
+    #ifndef UINT16
+        #define UINT16 unsigned short int
+    #endif
+
+    #ifndef UINT32
+        #define UINT32 unsigned int
+    #endif
+
+    #ifndef UINT64
+        #define UINT64 unsigned long long int
+    #endif
+
+    #ifndef INT8
+        #define INT8 char
+    #endif
+
+    #ifndef INT16
+        #define INT16 short int
+    #endif
+
+    #ifndef INT32
+        #define INT32 int
+    #endif
+
+    #ifndef INT64
+        #define INT64 long long int
+    #endif
+
     // --- alias types ---
     #ifndef TRUE
         #define TRUE  0xFF
