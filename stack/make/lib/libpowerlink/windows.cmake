1944d7157507981222b154a9bbb0a740d75211cd
Josef Baumgartner <josef.baumgartner@br-automation.com>
2013-05-27 16:43:56 +0200

    [TASK] Adapt powerlink libraries for new files
    - Adapt powerlink libraries to use new DLL CAL, event and PDO files
    - SharedBuff is no longer needed in libraries
    
    Change-Id: I418866a472eff3ad5066b188ea1aa6f7def226f6
    Reviewed-on: http://brateggevoat1/gerrit/1009
    Reviewed-by: Josef Baumgartner <josef.baumgartner@br-automation.com>
    Tested-by: Josef Baumgartner <josef.baumgartner@br-automation.com>

--- stack/make/lib/libpowerlink/windows.cmake
+++ stack/make/lib/libpowerlink/windows.cmake
@@ -43,11 +43,14 @@ SET (LIB_ARCH_SOURCES
      ${LIB_ARCH_SOURCES}
      ${EDRV_SOURCE_DIR}/edrv-pcap.c
      ${COMMON_SOURCE_DIR}/timer/timer-generic.c
-     ${LIB_SOURCE_DIR}/sharedbuff/ShbIpc-Win32.c
+     ${LIB_SOURCE_DIR}/circbuf/circbuf-win32.c
      ${LIB_SOURCE_DIR}/trace/trace-windows.c
      ${ARCH_SOURCE_DIR}/windows/target-windows.c
+     ${USER_SOURCE_DIR}/event/eventucal-win32.c
+     ${USER_SOURCE_DIR}/event/eventucalintf-circbuf.c
+     ${KERNEL_SOURCE_DIR}/event/eventkcal-win32.c
+     ${KERNEL_SOURCE_DIR}/event/eventkcalintf-circbuf.c
      )
-
 
 INCLUDE_DIRECTORIES(${INCLUDE_DIRECTORIES}
                     ${LIB_SOURCE_DIR}/pcap/windows/WpdPack/Include
--- stack/make/lib/libpowerlink_user/linux.cmake
+++ stack/make/lib/libpowerlink_user/linux.cmake
@@ -32,11 +32,38 @@
 ADD_DEFINITIONS(-Wall -Wextra -pedantic -std=c99 -pthread -D_GNU_SOURCE -D_POSIX_C_SOURCE=200112L
                 -fno-strict-aliasing)
 
-SET(LIB_ARCH_SOURCES ${USER_SOURCE_DIR}/pdo/pdoucalsync-bsdsem.c
-                     ${USER_SOURCE_DIR}/sdo/sdo-udpu.c
-                     ${COMMON_SOURCE_DIR}/timer/timer-linuxuser.c
-                     ${LIB_SOURCE_DIR}/sharedbuff/ShbIpc-LinuxUser.c
-                     ${ARCH_SOURCE_DIR}/linux/ftrace-debug.c
-                     ${ARCH_SOURCE_DIR}/linux/target-linux.c
-                     ${LIB_SOURCE_DIR}/trace/trace-printf.c
-        )
+SET(LIB_ARCH_SOURCES 
+     ${USER_SOURCE_DIR}/sdo/sdo-udpu.c
+     ${COMMON_SOURCE_DIR}/timer/timer-linuxuser.c
+     ${ARCH_SOURCE_DIR}/linux/ftrace-debug.c
+     ${ARCH_SOURCE_DIR}/linux/target-linux.c
+     ${LIB_SOURCE_DIR}/trace/trace-printf.c
+     )
+
+IF (CFG_KERNEL_STACK_KERNEL_MODULE)
+ADD_DEFINITIONS(-DCONFIG_USE_KERNEL_MODULE)
+SET (LIB_ARCH_SOURCES
+     ${LIB_ARCH_SOURCES}
+     ${USER_SOURCE_DIR}/pdo/pdoucalsync-ioctl.c
+     ${USER_SOURCE_DIR}/pdo/pdoucalmem-linuxmmap.c
+     ${USER_SOURCE_DIR}/dll/dllucal-ioctl.c
+     ${USER_SOURCE_DIR}/ctrl/ctrlucal-ioctl.c
+     ${USER_SOURCE_DIR}/event/eventucal-linuxioctl.c
+     ${USER_SOURCE_DIR}/errhnd/errhnducal-ioctl.c
+     )
+ELSE (CFG_KERNEL_STACK_KERNEL_MODULE)
+SET (LIB_ARCH_SOURCES
+     ${LIB_ARCH_SOURCES}
+     ${LIB_SOURCE_DIR}/circbuf/circbuf-linuxshm.c
+     ${USER_SOURCE_DIR}/pdo/pdoucalmem-posixshm.c
+     ${USER_SOURCE_DIR}/pdo/pdoucalsync-bsdsem.c
+     ${USER_SOURCE_DIR}/dll/dllucal-circbuf.c
+     ${USER_SOURCE_DIR}/ctrl/ctrlucal-mem.c
+     ${COMMON_SOURCE_DIR}/ctrl/ctrlcal-posixshm.c
+     ${USER_SOURCE_DIR}/event/eventucal-linux.c
+     ${USER_SOURCE_DIR}/event/eventucalintf-circbuf.c
+     ${USER_SOURCE_DIR}/errhnd/errhnducal-posixshm.c
+     )
+ENDIF(CFG_KERNEL_STACK_KERNEL_MODULE)
+
+
