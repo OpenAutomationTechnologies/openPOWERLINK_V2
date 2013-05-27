a7c890902f2bdf322ab4030dabf6f65c10085719
Josef Baumgartner <josef.baumgartner@br-automation.com>
2013-05-27 15:36:51 +0200

    [FEATURE] Add additional control CAL module implementations
    - Linux ioctl/kernel implementation
    - Posix shared memory implementation
    
    Change-Id: I560bd865f832024aa28fa796ab7c1c9bf44ae821

---
+++ stack/src/common/ctrl/ctrlcal-posixshm.c
@@ -0,0 +1,232 @@
+/**
+********************************************************************************
+\file   ctrlcal-posixshm.c
+
+\brief  Posix shared memory implementation for control CAL module
+
+The file contains a posix shared memory implementation which can be used by the
+memory block control CAL modules.
+
+\ingroup module_ctrl
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+Copyright (c) 2012, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
+
+//------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
+#include <Epl.h>
+
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <fcntl.h>           /* For O_* constants */
+#include <sys/stat.h>
+
+//============================================================================//
+//            G L O B A L   D E F I N I T I O N S                             //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+#define CTRL_SHM_NAME     "/shmCtrlCal"
+
+//------------------------------------------------------------------------------
+// module global vars
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// global function prototypes
+//------------------------------------------------------------------------------
+
+//============================================================================//
+//            P R I V A T E   D E F I N I T I O N S                           //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// local types
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// local vars
+//------------------------------------------------------------------------------
+static int          fd_l;
+static BYTE*        pCtrlMem_l;
+static int          size_l;
+static BOOL         fCreator_l;
+
+//------------------------------------------------------------------------------
+// local function prototypes
+//------------------------------------------------------------------------------
+
+//============================================================================//
+//            P U B L I C   F U N C T I O N S                                 //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+/**
+\brief    Initialize control CAL module
+
+The function initializes the control CAL module.
+
+\param  size_p      The size of the memory control block.
+
+\return The function returns a tEplKernel error code.
+
+\ingroup module_ctrlcal
+*/
+//------------------------------------------------------------------------------
+tEplKernel ctrlcal_init(UINT size_p)
+{
+    struct stat             stat;
+
+    if ((fd_l = shm_open(CTRL_SHM_NAME, O_RDWR | O_CREAT, 0)) < 0)
+    {
+        EPL_DBGLVL_ERROR_TRACE("%s() shm_open failed!\n", __func__);
+        return kEplNoResource;
+    }
+
+    if (fstat(fd_l, &stat) != 0)
+    {
+        close (fd_l);
+        return kEplNoResource;
+    }
+
+    if (stat.st_size == 0)
+    {
+        if (ftruncate(fd_l, size_p) == -1)
+        {
+            EPL_DBGLVL_ERROR_TRACE("%s() ftruncate failed!\n", __func__);
+            close (fd_l);
+            shm_unlink(CTRL_SHM_NAME);
+            return kEplNoResource;
+        }
+        fCreator_l = TRUE;
+    }
+
+    pCtrlMem_l = mmap(NULL, size_p, PROT_READ | PROT_WRITE, MAP_SHARED, fd_l, 0);
+    if (pCtrlMem_l == MAP_FAILED)
+    {
+        EPL_DBGLVL_ERROR_TRACE("%s() mmap header failed!\n", __func__);
+        close (fd_l);
+        if (fCreator_l)
+            shm_unlink(CTRL_SHM_NAME);
+        return kEplNoResource;
+    }
+
+    if (fCreator_l)
+    {
+        EPL_MEMSET(pCtrlMem_l, 0, size_p);
+    }
+    size_l = size_p;
+    return kEplSuccessful;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief    Cleanup control module
+
+The function cleans up the control CAL module.
+
+\return The function returns a tEplKernel error code.
+
+\ingroup module_ctrlcal
+*/
+//------------------------------------------------------------------------------
+tEplKernel ctrlcal_exit (void)
+{
+    tEplKernel      ret = kEplSuccessful;
+
+    if (pCtrlMem_l != NULL)
+    {
+        munmap(pCtrlMem_l, size_l);
+        close(fd_l);
+        if (fCreator_l)
+            shm_unlink(CTRL_SHM_NAME);
+        fd_l = 0;
+        pCtrlMem_l = 0;
+        size_l = 0;
+    }
+    return ret;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief Write data to control block
+
+The function writes data to the control block.
+
+\param  offset_p            Offset in memory block to store the data.
+\param  pSrc_p              Pointer to the data which should be stored.
+\param  length_p            The length of the data to be stored.
+
+\ingroup module_ctrlcal
+*/
+//------------------------------------------------------------------------------
+void ctrlcal_writeData(UINT offset_p, void* pSrc_p, size_t length_p)
+{
+    if (pCtrlMem_l == NULL)
+    {
+        EPL_DBGLVL_ERROR_TRACE ("%s() instance == NULL!\n", __func__);
+        return;
+    }
+    EPL_MEMCPY(pCtrlMem_l + offset_p, pSrc_p, length_p);
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief Read data from control block
+
+The function reads data from the control block.
+
+\param  pDest_p             Pointer to store the read data.
+\param  offset_p            Offset in memory block from which to read.
+\param  length_p            The length of the data to be read.
+
+\return The function returns a tEplKernel error code.
+
+\ingroup module_ctrlcal
+*/
+//------------------------------------------------------------------------------
+tEplKernel ctrlcal_readData(void* pDest_p, UINT offset_p, size_t length_p)
+{
+    if (pCtrlMem_l == NULL)
+    {
+        EPL_DBGLVL_ERROR_TRACE ("%s() pCtrlMem_l == NULL!\n", __func__);
+        return kEplGeneralError;
+    }
+
+    EPL_MEMCPY(pDest_p, pCtrlMem_l + offset_p, length_p);
+    return kEplSuccessful;
+}
+
---
+++ stack/src/user/ctrl/ctrlucal-ioctl.c
@@ -0,0 +1,358 @@
+/**
+********************************************************************************
+\file   ctrlucal-ioctl.c
+
+\brief  User control CAL module using a ioctl calls to Linux kernel
+
+This file contains an implementation of the user control CAL module which uses
+ioctl calls for communication with the kernel layer.
+
+\ingroup module_ctrlucal
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+Copyright (c) 2013, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
+
+//------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
+#include <unistd.h>
+#include <stddef.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+#include <Epl.h>
+#include <EplTarget.h>
+#include <ctrl.h>
+#include <ctrlcal.h>
+#include <ctrlcal-mem.h>
+#include <user/ctrlucal.h>
+
+#include <powerlink-module.h>
+
+
+
+//============================================================================//
+//            G L O B A L   D E F I N I T I O N S                             //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+#define CMD_TIMEOUT_CNT     100     // loop counter for command timeout
+
+//------------------------------------------------------------------------------
+// module global vars
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// global function prototypes
+//------------------------------------------------------------------------------
+
+//============================================================================//
+//            P R I V A T E   D E F I N I T I O N S                           //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// local types
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// local vars
+//------------------------------------------------------------------------------
+static int fd_l;           // file descriptor for powerlink device
+
+//------------------------------------------------------------------------------
+// local function prototypes
+//------------------------------------------------------------------------------
+UINT16 getMagic (void);
+
+//============================================================================//
+//            P U B L I C   F U N C T I O N S                                 //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+/**
+\brief  Initialize user control CAL module
+
+The function initializes the user control CAL module.
+
+\return The function returns a tEplKernel error code.
+
+\ingroup module_ctrlucal
+*/
+//------------------------------------------------------------------------------
+tEplKernel ctrlucal_init(void)
+{
+    if ((fd_l = open(PLK_DEV_FILE, O_RDWR)) < 0)
+    {
+        TRACE("%s() open return error %d (%s)\n", __func__, fd_l, strerror(fd_l));
+        return kEplNoResource;
+    }
+
+    return kEplSuccessful;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Cleanup user control CAL module
+
+The function cleans-up the user control CAL module.
+
+\ingroup module_ctrlucal
+*/
+//------------------------------------------------------------------------------
+void ctrlucal_exit (void)
+{
+    close (fd_l);
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Process user control CAL module
+
+This function provides processing time for the CAL module.
+
+\return The function returns a tEplKernel error code.
+
+\ingroup module_ctrlucal
+*/
+//------------------------------------------------------------------------------
+tEplKernel ctrlucal_process (void)
+{
+    return kEplSuccessful;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief    Execute a ctrl command
+
+The function executes a control command in the kernel stack.
+
+\param  cmd_p            Command to execute
+
+\return The function returns a tEplKernel error code.
+
+\ingroup module_ctrlucal
+*/
+//------------------------------------------------------------------------------
+tEplKernel ctrlucal_executeCmd(tCtrlCmdType cmd_p)
+{
+    tCtrlCmd            ctrlCmd;
+    int                 ret;
+
+    ctrlCmd.cmd = cmd_p;
+    ctrlCmd.retVal = 0;
+
+    if ((ret = ioctl (fd_l, PLK_CMD_CTRL_EXECUTE_CMD, &ctrlCmd)) != 0)
+    {
+        EPL_DBGLVL_ERROR_TRACE("%s() ioctl error %d\n", __func__, ret);
+        return kEplGeneralError;
+    }
+
+    return ctrlCmd.retVal;
+}
+
+
+//------------------------------------------------------------------------------
+/**
+\brief Check state of kernel stack
+
+The function checks the state of the kernel stack. If it is already running
+it tries to shutdown.
+
+\return The function returns a tEplKernel error code.
+\retval kEplSuccessful  If kernel stack is initialized
+\retval kEplNoResource  If kernel stack is not running or in wrong state
+
+\ingroup module_ctrlucal
+*/
+//------------------------------------------------------------------------------
+tEplKernel ctrlucal_checkKernelStack(void)
+{
+    UINT16              kernelStatus;
+    tEplKernel          ret;
+
+    TRACE ("Checking for kernel stack...\n");
+    kernelStatus = ctrlucal_getStatus();
+
+    switch(kernelStatus)
+    {
+        case kCtrlStatusReady:
+            ret = kEplSuccessful;
+            break;
+
+        case kCtrlStatusRunning:
+            /* try to shutdown kernel stack */
+            ret = ctrlucal_executeCmd(kCtrlCleanupStack);
+            if (ret != kEplSuccessful)
+            {
+                ret = kEplNoResource;
+                break;
+            }
+
+            target_msleep(1000);
+
+            kernelStatus = ctrlucal_getStatus();
+            if (kernelStatus != kCtrlStatusReady)
+            {
+                ret = kEplNoResource;
+            }
+            break;
+
+        default:
+            ret = kEplNoResource;
+            break;
+    }
+
+    return ret;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief    Get status of kernel stack
+
+The function gets the status of the kernel stack
+
+\return The function returns the kernel status.
+
+\ingroup module_ctrlucal
+*/
+//------------------------------------------------------------------------------
+UINT16 ctrlucal_getStatus(void)
+{
+    int         ret;
+    UINT16      status;
+
+    if ((ret = ioctl (fd_l, PLK_CMD_CTRL_GET_STATUS, &status)) != 0)
+    {
+        EPL_DBGLVL_ERROR_TRACE("%s() ioctl error %d\n", __func__, ret);
+        return kCtrlStatusUnavailable;
+    }
+    return status;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief Get the heartbeat of the kernel stack
+
+The function reads the heartbeat genereated by the kernel stack.
+
+\return The function returns the heartbeat counter.
+
+\ingroup module_ctrlucal
+*/
+//------------------------------------------------------------------------------
+UINT16 ctrlucal_getHeartbeat(void)
+{
+    int         ret;
+    UINT16      heartbeat;
+
+    if ((ret = ioctl (fd_l, PLK_CMD_CTRL_GET_HEARTBEAT, &heartbeat)) != 0)
+    {
+        TRACE("%s() error %d\n", __func__, ret);
+        return 0;
+    }
+    return heartbeat;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Store the init parameters for kernel use
+
+The function stores the openPOWERLINK initialization parameter so that they
+can be accessed by the kernel stack.
+
+\param  pInitParam_p        Specifies where to read the init parameters.
+
+\ingroup module_ctrlucal
+*/
+//------------------------------------------------------------------------------
+void ctrlucal_storeInitParam(tCtrlInitParam* pInitParam_p)
+{
+    int                 ret;
+
+    if ((ret = ioctl (fd_l, PLK_CMD_CTRL_STORE_INITPARAM, pInitParam_p)) != 0)
+    {
+        EPL_DBGLVL_ERROR_TRACE("%s() ioctl error %d\n", __func__, ret);
+    }
+
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Read the init parameters from kernel
+
+The function reads the initialization parameter from the kernel stack.
+
+\param  pInitParam_p        Specifies where to store the read init parameters.
+
+\return The function returns a tEplKernel error code. It returns always
+        kEplSuccessful!
+
+\ingroup module_ctrlucal
+*/
+//------------------------------------------------------------------------------
+tEplKernel ctrlucal_readInitParam(tCtrlInitParam* pInitParam_p)
+{
+    int                 ret;
+
+    if ((ret = ioctl (fd_l, PLK_CMD_CTRL_READ_INITPARAM, pInitParam_p)) != 0)
+    {
+        EPL_DBGLVL_ERROR_TRACE("%s() ioctl error %d\n", __func__, ret);
+        return kEplGeneralError;
+    }
+    return kEplSuccessful;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Return the file descriptor of the kernel module
+
+The function returns the file descriptor of the kernel module.
+
+\return The function returns the file descriptor.
+
+\ingroup module_ctrlucal
+*/
+//------------------------------------------------------------------------------
+int ctrlucal_getFd(void)
+{
+    return fd_l;
+}
+
+//============================================================================//
+//            P R I V A T E   F U N C T I O N S                               //
+//============================================================================//
+
