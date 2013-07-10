b8b23bba164650db86b9921fa08d2f521c59d734
Josef Baumgartner <josef.baumgartner@br-automation.com>
2013-07-10 11:01:13 +0200

    [TASK] Rework NMT MNU module (7)
    - Add doxygen file and function headers
    
    Change-Id: If0b41af3ae13d6c4cfcb9980536ee83601afe39b
    Reviewed-on: http://brateggevoat1/gerrit/1135
    Tested-by: Jenkins <jenkins@brateggevoat1.br-automation.co.at>
    Reviewed-by: Joerg Zelenka <joerg.zelenka@br-automation.com>
    Reviewed-by: Josef Baumgartner <josef.baumgartner@br-automation.com>

--- include/user/nmtmnu.h
+++ include/user/nmtmnu.h
@@ -1,88 +1,56 @@
-/****************************************************************************
+/**
+********************************************************************************
+\file   nmtmnu.h
 
-  (c) SYSTEC electronic GmbH, D-07973 Greiz, August-Bebel-Str. 29
-      www.systec-electronic.com
+\brief  Definitions for nmtmnu module
 
-  Project:      openPOWERLINK
+This file contains the definitions for the nmtmnu module.
 
-  Description:  include file for NMT-MN-Userspace-Module
+*******************************************************************************/
 
-  License:
+/*------------------------------------------------------------------------------
+Copyright (c) 2013, SYSTEC electronic GmbH
+Copyright (c) 2013, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+All rights reserved.
 
-    Redistribution and use in source and binary forms, with or without
-    modification, are permitted provided that the following conditions
-    are met:
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
 
-    1. Redistributions of source code must retain the above copyright
-       notice, this list of conditions and the following disclaimer.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
 
-    2. Redistributions in binary form must reproduce the above copyright
-       notice, this list of conditions and the following disclaimer in the
-       documentation and/or other materials provided with the distribution.
+#ifndef _INC_nmtmnu_H_
+#define _INC_nmtmnu_H_
 
-    3. Neither the name of SYSTEC electronic GmbH nor the names of its
-       contributors may be used to endorse or promote products derived
-       from this software without prior written permission. For written
-       permission, please contact info@systec-electronic.com.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-    COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-    POSSIBILITY OF SUCH DAMAGE.
-
-    Severability Clause:
-
-        If a provision of this License is or becomes illegal, invalid or
-        unenforceable in any jurisdiction, that shall not affect:
-        1. the validity or enforceability in that jurisdiction of any other
-           provision of this License; or
-        2. the validity or enforceability in other jurisdictions of that or
-           any other provision of this License.
-
-  -------------------------------------------------------------------------
-
-                $RCSfile$
-
-                $Author$
-
-                $Revision$  $Date$
-
-                $State$
-
-                Build Environment:
-                    GCC V3.4
-
-  -------------------------------------------------------------------------
-
-  Revision History:
-
-  2006/06/09 k.t.:   start of the implementation
-
-
-****************************************************************************/
-
-#ifndef _NMTMNU_H_
-#define _NMTMNU_H_
-
+//------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
 #include "EplNmtu.h"
 
-
-//---------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // const defines
-//---------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
-
-//---------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // typedef
-//---------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
 typedef tEplKernel (*tNmtMnuCbNodeEvent) (UINT nodeId_p, tEplNmtNodeEvent NodeEvent_p,
                                           tEplNmtState NmtState_p, UINT16 wErrorCode_p,
@@ -97,38 +65,26 @@ typedef struct
     UINT32   prcPResTimeFirstNegOffsetNs;
 } tEplNmtMnuConfigParam;
 
-
-//---------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // function prototypes
-//---------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #if(((EPL_MODULE_INTEGRATION) & (EPL_MODULE_NMT_MN)) != 0)
 
-tEplKernel nmtmnu_init(tNmtMnuCbNodeEvent pfnCbNodeEvent_p,
-                       tNmtMnuCbBootEvent pfnCbBootEvent_p);
-
-tEplKernel nmtmnu_addInstance(tNmtMnuCbNodeEvent pfnCbNodeEvent_p,
-                              tNmtMnuCbBootEvent pfnCbBootEvent_p);
-
+tEplKernel nmtmnu_init(tNmtMnuCbNodeEvent pfnCbNodeEvent_p, tNmtMnuCbBootEvent pfnCbBootEvent_p);
+tEplKernel nmtmnu_addInstance(tNmtMnuCbNodeEvent pfnCbNodeEvent_p, tNmtMnuCbBootEvent pfnCbBootEvent_p);
 tEplKernel nmtmnu_delInstance(void);
-
-EPLDLLEXPORT tEplKernel nmtmnu_processEvent(tEplEvent* pEvent_p);
-
-tEplKernel nmtmnu_sendNmtCommand(UINT nodeId_p,
-                                   tEplNmtCommand  nmtCommand_p);
-
-tEplKernel nmtmnu_requestNmtCommand(UINT nodeId_p,
-                                    tEplNmtCommand  nmtCommand_p);
-
-tEplKernel nmtmnu_triggerStateChange(UINT nodeId_p,
-                                       tEplNmtNodeCommand  nodeCommand_p);
-
+tEplKernel nmtmnu_processEvent(tEplEvent* pEvent_p);
+tEplKernel nmtmnu_sendNmtCommand(UINT nodeId_p, tEplNmtCommand  nmtCommand_p);
+tEplKernel nmtmnu_requestNmtCommand(UINT nodeId_p, tEplNmtCommand nmtCommand_p);
+tEplKernel nmtmnu_triggerStateChange(UINT nodeId_p, tEplNmtNodeCommand nodeCommand_p);
 tEplKernel nmtmnu_cbNmtStateChange(tEplEventNmtStateChange nmtStateChange_p);
-
 tEplKernel nmtmnu_cbCheckEvent(tEplNmtEvent NmtEvent_p);
-
-tEplKernel nmtmnu_getDiagnosticInfo(UINT* pMandatorySlaveCount_p,
-                                    UINT* pSignalSlaveCount_p,
+tEplKernel nmtmnu_getDiagnosticInfo(UINT* pMandatorySlaveCount_p, UINT* pSignalSlaveCount_p,
                                     UINT16* pflags_p);
 
 #if EPL_NMTMNU_PRES_CHAINING_MN != FALSE
@@ -137,6 +93,8 @@ tEplKernel nmtmnu_configPrc(tEplNmtMnuConfigParam* pConfigParam_p);
 
 #endif
 
-#endif  // #ifndef _NMTMNU_H_
+#ifdef __cplusplus
+}
+#endif
 
-
+#endif /* _INC_nmtmnu_H_ */
