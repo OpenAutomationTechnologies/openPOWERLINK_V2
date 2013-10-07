1d5d8e6472847db21f53832a3c4ceea91a5ac9bd
Joerg Zelenka <joerg.zelenka@br-automation.com>
2013-10-07 17:26:20 +0200

    [FEATURE] Add common GPIO library for embedded demos
    The GPIO library provides a common interface to:
    - Node ID switch
    - POWERLINK S/E LED
    - Application I/Os
    
    Change-Id: I0c3d3536365d1039fdb478b64baa38419bb6dc60
    Reviewed-on: http://brateggevoat1/gerrit/1384
    Tested-by: Jenkins <jenkins@brateggevoat1.br-automation.co.at>
    Reviewed-by: Thomas Mair <thomas.mair@br-automation.com>
    Reviewed-by: Josef Baumgartner <josef.baumgartner@br-automation.com>

---
+++ examples/common/gpio/gpio-nios2.c
@@ -0,0 +1,231 @@
+/**
+********************************************************************************
+\file   gpio-nios2.c
+
+\brief  GPIOs for Altera Nios II
+
+The file implements the GPIOs on Altera Nios II used by openPOWERLINK demo
+applications.
+
+\ingroup module_app_common
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+Copyright (c) 2013, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+Copyright (c) 2013, SYSTEC electronic GmbH
+Copyright (c) 2013, Kalycito Infotech Private Ltd.All rights reserved.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
+
+//------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
+#include <Epl.h>
+
+#include "gpio.h"
+
+#include <system.h>
+#include <altera_avalon_pio_regs.h>
+
+//============================================================================//
+//            G L O B A L   D E F I N I T I O N S                             //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// module global vars
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// global function prototypes
+//------------------------------------------------------------------------------
+
+//============================================================================//
+//            P R I V A T E   D E F I N I T I O N S                           //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+#define GPIO_STATUS_LED_BIT     1
+#define GPIO_ERROR_LED_BIT      2
+
+//------------------------------------------------------------------------------
+// local types
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// local vars
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// local function prototypes
+//------------------------------------------------------------------------------
+
+//============================================================================//
+//            P U B L I C   F U N C T I O N S                                 //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+/**
+\brief  Initialize GPIO module
+
+The function initializes the GPIO module before being used.
+
+\return Returns 0 if successful
+
+\ingroup module_app_common
+*/
+//------------------------------------------------------------------------------
+int gpio_init(void)
+{
+    return 0;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Shutdown GPIO module
+
+The function shuts down the GPIO module.
+
+\ingroup module_app_common
+*/
+//------------------------------------------------------------------------------
+void gpio_shutdown(void)
+{
+
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Gets the node switch value
+
+The function returns the node ID set by the node switches.
+
+\return Returns the set node ID
+
+\ingroup module_app_common
+*/
+//------------------------------------------------------------------------------
+UINT8 gpio_getNodeid(void)
+{
+    UINT8 nodeid;
+#ifdef NODE_SWITCH_PIO_BASE
+    nodeid = IORD_ALTERA_AVALON_PIO_DATA(NODE_SWITCH_PIO_BASE);
+#else
+    nodeid = 0;
+#endif
+
+    return nodeid;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Sets the status LED
+
+The function sets the POWERLINK status LED.
+
+\param  fOn_p               Determines the LED state
+
+\ingroup module_app_common
+*/
+//------------------------------------------------------------------------------
+void gpio_setStatusLed(BOOL fOn_p)
+{
+    if(fOn_p != FALSE)
+        IOWR_ALTERA_AVALON_PIO_SET_BITS(STATUS_LED_PIO_BASE, GPIO_STATUS_LED_BIT);
+    else
+        IOWR_ALTERA_AVALON_PIO_CLEAR_BITS(STATUS_LED_PIO_BASE, GPIO_STATUS_LED_BIT);
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Sets the error LED
+
+The function sets the POWERLINK error LED.
+
+\param  fOn_p               Determines the LED state
+
+\ingroup module_app_common
+*/
+//------------------------------------------------------------------------------
+void gpio_setErrorLed(BOOL fOn_p)
+{
+    if(fOn_p != FALSE)
+        IOWR_ALTERA_AVALON_PIO_SET_BITS(STATUS_LED_PIO_BASE, GPIO_ERROR_LED_BIT);
+    else
+        IOWR_ALTERA_AVALON_PIO_CLEAR_BITS(STATUS_LED_PIO_BASE, GPIO_ERROR_LED_BIT);
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Gets the application input
+
+The function returns application inputs.
+
+\return Returns the application inputs.
+
+\ingroup module_app_common
+*/
+//------------------------------------------------------------------------------
+UINT8 gpio_getAppInput(void)
+{
+    UINT8 key;
+#ifdef KEY_PIO_BASE
+    key = IORD_ALTERA_AVALON_PIO_DATA(KEY_PIO_BASE);
+#else
+    key = 0;
+#endif
+
+    return key;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Sets the application output
+
+The function sets the application outputs.
+
+\param  val_p               Determines the value to be set to the output
+
+\ingroup module_app_common
+*/
+//------------------------------------------------------------------------------
+void gpio_setAppOutputs(UINT32 val_p)
+{
+#ifdef HEX_PIO_BASE
+    IOWR_ALTERA_AVALON_PIO_DATA(HEX_PIO_BASE, val_p);
+#endif
+}
+
+//============================================================================//
+//            P R I V A T E   F U N C T I O N S                               //
+//============================================================================//
+/// \name Private Functions
+/// \{
+///\}
---
+++ examples/common/gpio/gpio.h
@@ -0,0 +1,79 @@
+/**
+********************************************************************************
+\file   gpio.h
+
+\brief  Definitions for GPIOs
+
+This header file provides the definitions for GPIOs used by the openPOWERLINK 
+examples.
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+Copyright (c) 2013, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+Copyright (c) 2013, SYSTEC electronic GmbH
+Copyright (c) 2013, Kalycito Infotech Private Ltd.All rights reserved.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
+
+#ifndef _INC_gpio_H_
+#define _INC_gpio_H_
+
+//------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
+#include <Epl.h>
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// typedef
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// function prototypes
+//------------------------------------------------------------------------------
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int gpio_init(void);
+void gpio_shutdown(void);
+
+UINT8 gpio_getNodeid(void);
+
+void gpio_setStatusLed(BOOL fOn_p);
+void gpio_setErrorLed(BOOL fOn_p);
+
+UINT8 gpio_getAppInput(void);
+void gpio_setAppOutputs(UINT32 val_p);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _INC_gpio_H_ */
