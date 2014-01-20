0a8186b17cf4dd538a5e70fbf820bd08dd786497
Josef Baumgartner <josef.baumgartner@br-automation.com>
2014-01-20 12:37:49 +0100

    [TASK] Refactor demo_mn_qt
    - Refactor CMake files
    - Add local build directory
    
    Change-Id: I8cd3aee5e637e878017f94b0f7e9806da04a822f
    Reviewed-on: http://brateggevoat1/gerrit/1807
    Tested-by: Jenkins <jenkins@brateggevoat1.br-automation.co.at>
    Reviewed-by: Wolfgang Seiss <wolfgang.seiss@br-automation.com>
    Reviewed-by: Josef Baumgartner <josef.baumgartner@br-automation.com>

--- apps/demo_mn_qt/windows.cmake
+++ apps/demo_mn_qt/windows.cmake
@@ -1,8 +1,8 @@
 ################################################################################
 #
-# Windows definitions for console QT application
+# Windows definitions for demo_mn_qt application
 #
-# Copyright (c) 2013, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+# Copyright (c) 2014, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -27,26 +27,38 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ################################################################################
+
+################################################################################
+# Set architecture specific definitions
 ADD_DEFINITIONS(-D_CONSOLE -DWPCAP -DHAVE_REMOTE -D_CRT_SECURE_NO_WARNINGS)
 
-SET (DEMO_ARCH_SOURCES
-     ${DEMO_ARCH_SOURCES}
-     ${LIB_SOURCE_DIR}/trace/trace-windows.c
-     ${ARCH_SOURCE_DIR}/windows/target-windows.c
-     )
+################################################################################
+# Set architecture specific sources and include directories
 
-INCLUDE_DIRECTORIES(${LIB_SOURCE_DIR}/pcap/windows/WpdPack/Include)
+SET(DEMO_ARCH_SOURCES
+    ${DEMO_ARCH_SOURCES}
+    ${LIB_SOURCE_DIR}/trace/trace-windows.c
+    ${ARCH_SOURCE_DIR}/windows/target-windows.c
+    )
 
-IF (CMAKE_CL_64)
-    LINK_DIRECTORIES(${LIB_SOURCE_DIR}/pcap/windows/WpdPack/Lib/x64)
-ELSE (CMAKE_CL_64)
-    LINK_DIRECTORIES(${LIB_SOURCE_DIR}/pcap/windows/WpdPack/Lib)
-ENDIF (CMAKE_CL_64)
+INCLUDE_DIRECTORIES(${CONTRIB_SOURCE_DIR}/pcap/windows/WpdPack/Include)
 
-SET (ARCH_LIBRARIES ${ARCH_LIBRARIES} wpcap iphlpapi)
+################################################################################
+# Set architecture specific libraries
 
-INSTALL(FILES ${QT_BINARY_DIR}/QtCore4.dll DESTINATION bin)
-INSTALL(FILES ${QT_BINARY_DIR}/QtGui4.dll DESTINATION bin)
-INSTALL(FILES ${QT_BINARY_DIR}/QtCored4.dll DESTINATION bin)
-INSTALL(FILES ${QT_BINARY_DIR}/QtGuid4.dll DESTINATION bin)
+IF(CMAKE_CL_64)
+    LINK_DIRECTORIES(${CONTRIB_SOURCE_DIR}/pcap/windows/WpdPack/Lib/x64)
+ELSE()
+    LINK_DIRECTORIES(${CONTRIB_SOURCE_DIR}/pcap/windows/WpdPack/Lib)
+ENDIF()
+
+SET(ARCH_LIBRARIES ${ARCH_LIBRARIES} wpcap iphlpapi)
+
+################################################################################
+# Set architecture specific installation files
+
+INSTALL(FILES ${QT_BINARY_DIR}/QtCore4.dll DESTINATION ${CMAKE_PROJECT_NAME})
+INSTALL(FILES ${QT_BINARY_DIR}/QtGui4.dll DESTINATION ${CMAKE_PROJECT_NAME})
+INSTALL(FILES ${QT_BINARY_DIR}/QtCored4.dll DESTINATION ${CMAKE_PROJECT_NAME})
+INSTALL(FILES ${QT_BINARY_DIR}/QtGuid4.dll DESTINATION ${CMAKE_PROJECT_NAME})
 
