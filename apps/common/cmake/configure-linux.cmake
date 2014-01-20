1b03d4d19254d77659bf5b0b27eba7b5960a454d
Josef Baumgartner <josef.baumgartner@br-automation.com>
2014-01-20 12:34:58 +0100

    [TASK] Add generic CMake configuration files for the demos
    Change-Id: I7060882a6f49a0e8ff76bac6c98c796b8fcc64aa
    Reviewed-on: http://brateggevoat1/gerrit/1804
    Tested-by: Jenkins <jenkins@brateggevoat1.br-automation.co.at>
    Reviewed-by: Wolfgang Seiss <wolfgang.seiss@br-automation.com>
    Reviewed-by: Josef Baumgartner <josef.baumgartner@br-automation.com>

---
+++ apps/common/cmake/configure-linux.cmake
@@ -0,0 +1,71 @@
+################################################################################
+#
+# Linux configuration options for openPOWERLINK demo applications
+#
+# Copyright (c) 2014, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the copyright holders nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+################################################################################
+
+SET(CFG_BUILD_KERNEL_STACK "Link to Application"
+    CACHE STRING "Configure how to build the kernel stack")
+
+SET(KernelStackBuildTypes
+    "Link to Application;Linux Userspace Daemon;Linux Kernel Module;None"
+    CACHE INTERNAL
+    "List of possible kernel stack build types")
+
+SET_PROPERTY(CACHE CFG_BUILD_KERNEL_STACK
+             PROPERTY STRINGS ${KernelStackBuildTypes})
+
+IF (CFG_BUILD_KERNEL_STACK STREQUAL "Link to Application")
+
+    SET (CFG_KERNEL_STACK_DIRECTLINK ON CACHE INTERNAL
+         "Link kernel stack directly into application (Single process solution)")
+    UNSET (CFG_KERNEL_STACK_USERSPACE_DAEMON CACHE)
+    UNSET (CFG_KERNEL_STACK_KERNEL_MODULE CACHE)
+
+ELSEIF (CFG_BUILD_KERNEL_STACK STREQUAL "Linux Userspace Daemon")
+
+    SET (CFG_KERNEL_STACK_USERSPACE_DAEMON ON CACHE INTERNAL
+         "Build kernel stack as Linux userspace daemon")
+    UNSET (CFG_KERNEL_STACK_DIRECTLINK CACHE)
+    UNSET (CFG_KERNEL_STACK_KERNEL_MODULE CACHE)
+
+ELSEIF (CFG_BUILD_KERNEL_STACK STREQUAL "Linux Kernel Module")
+
+    SET (CFG_KERNEL_STACK_KERNEL_MODULE ON CACHE INTERNAL
+         "Build kernel stack as Linux kernelspace module")
+    UNSET (CFG_KERNEL_STACK_USERSPACE_DAEMON CACHE)
+    UNSET (CFG_KERNEL_STACK_DIRECTLINK CACHE)
+    UNSET (CFG_KERNEL_STACK_DIRECTLINK CACHE)
+
+ELSEIF (CFG_BUILD_KERNEL_STACK STREQUAL "None")
+    UNSET (CFG_KERNEL_STACK_USERSPACE_DAEMON CACHE)
+    UNSET (CFG_KERNEL_STACK_KERNEL_MODULE CACHE)
+
+ENDIF (CFG_BUILD_KERNEL_STACK STREQUAL "Link to Application")
+
+
+
---
+++ apps/common/cmake/options.cmake
@@ -0,0 +1,92 @@
+################################################################################
+#
+# Generic CMake options openPOWERLINK demo applications
+#
+# Copyright (c) 2014, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the copyright holders nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+################################################################################
+
+MESSAGE(STATUS "CMAKE_SYSTEM_NAME is ${CMAKE_SYSTEM_NAME}")
+MESSAGE(STATUS "CMAKE_SYSTEM_PROCESSOR is ${CMAKE_SYSTEM_PROCESSOR}")
+
+################################################################################
+# Set global directories
+################################################################################
+SET(OPLK_ROOT_DIR ${CMAKE_SOURCE_DIR}/../..)
+SET(DEMO_SOURCE_DIR ${CMAKE_SOURCE_DIR}/src)
+SET(DEMO_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/include)
+SET(COMMON_SOURCE_DIR ${CMAKE_SOURCE_DIR}/../common/src)
+SET(CONTRIB_SOURCE_DIR ${OPLK_ROOT_DIR}/contrib)
+SET(OPLK_INCLUDE_DIR ${OPLK_ROOT_DIR}/include)
+SET(TOOLS_DIR ${OPLK_ROOT_DIR}/tools)
+
+################################################################################
+# Include CMake Modules
+################################################################################
+SET(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/../common/cmake ${CMAKE_MODULE_PATH})
+
+# include standard cmake modules
+INCLUDE(CMakeDependentOption)
+
+# include project specific modules
+INCLUDE(findoplklib)
+
+################################################################################
+# Set options
+################################################################################
+
+STRING(TOLOWER "${CMAKE_SYSTEM_NAME}" SYSTEM_DIR_NAME)
+
+IF(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+  SET(CMAKE_INSTALL_PREFIX
+    ${OPLK_ROOT_DIR}/bin/${SYSTEM_DIR_NAME}/${CMAKE_SYSTEM_PROCESSOR} CACHE PATH "openPOWERLINK apps install prefix" FORCE
+    )
+ENDIF()
+
+SET(CMAKE_CONFIGURATION_TYPES "Debug;Release" CACHE INTERNAL "Available Build Configurations" FORCE)
+
+IF(NOT CMAKE_GENERATOR MATCHES "Visual Studio")
+    IF(NOT CMAKE_BUILD_TYPE)
+      SET(CMAKE_BUILD_TYPE Release CACHE STRING
+          "Choose the type of build, options are: None Debug Release"
+          FORCE)
+    ENDIF()
+ENDIF()
+
+SET(CFG_DEBUG_LVL "0xEC000000L" CACHE STRING "Debug Level for debug output")
+
+# set global include directories
+INCLUDE_DIRECTORIES (
+    ${OPLK_INCLUDE_DIR}
+    ${CONTRIB_SOURCE_DIR}
+    ${COMMON_SOURCE_DIR}
+)
+
+IF(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    INCLUDE(configure-linux)
+ELSEIF(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    INCLUDE(configure-windows)
+ENDIF()
+
