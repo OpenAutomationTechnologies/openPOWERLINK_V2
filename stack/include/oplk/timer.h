733955ced8fe019c7020fd3b7e42a5cedf855991
Josef Baumgartner <josef.baumgartner@br-automation.com>
2014-02-05 11:03:24 +0100

    [TASK] Cleanup timer.h
    Change-Id: Id38ddf9a4937f1cdbdd5dbeeb4de0f6059444db0
    Reviewed-on: http://brateggevoat1/gerrit/1936
    Tested-by: Jenkins <jenkins@brateggevoat1.br-automation.co.at>
    Reviewed-by: Joerg Zelenka <joerg.zelenka@br-automation.com>
    Reviewed-by: Thomas Mair <thomas.mair@br-automation.com>
    Reviewed-by: Josef Baumgartner <josef.baumgartner@br-automation.com>

--- stack/include/oplk/timer.h
+++ stack/include/oplk/timer.h
@@ -1,97 +1,62 @@
-/****************************************************************************
+/**
+********************************************************************************
+\file   timer.h
 
-  (c) SYSTEC electronic GmbH, D-07973 Greiz, August-Bebel-Str. 29
-      www.systec-electronic.com
+\brief  Generic definitions for timer modules
 
-  Project:      openPOWERLINK
+This file contains some generic definitions for timer modules.
+*******************************************************************************/
 
-  Description:  include file for Epl Timer-Module
+/*------------------------------------------------------------------------------
+Copyright (c) 2014, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+Copyright (c) 2013, SYSTEC electronic GmbH
+All rights reserved.
 
-  License:
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
 
-    Redistribution and use in source and binary forms, with or without
-    modification, are permitted provided that the following conditions
-    are met:
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
 
-    1. Redistributions of source code must retain the above copyright
-       notice, this list of conditions and the following disclaimer.
+#ifndef _INC_timer_H_
+#define _INC_timer_H_
 
-    2. Redistributions in binary form must reproduce the above copyright
-       notice, this list of conditions and the following disclaimer in the
-       documentation and/or other materials provided with the distribution.
-
-    3. Neither the name of SYSTEC electronic GmbH nor the names of its
-       contributors may be used to endorse or promote products derived
-       from this software without prior written permission. For written
-       permission, please contact info@systec-electronic.com.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-    COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-    POSSIBILITY OF SUCH DAMAGE.
-
-    Severability Clause:
-
-        If a provision of this License is or becomes illegal, invalid or
-        unenforceable in any jurisdiction, that shall not affect:
-        1. the validity or enforceability in that jurisdiction of any other
-           provision of this License; or
-        2. the validity or enforceability in other jurisdictions of that or
-           any other provision of this License.
-
-  -------------------------------------------------------------------------
-
-                $RCSfile$
-
-                $Author$
-
-                $Revision$  $Date$
-
-                $State$
-
-                Build Environment:
-                    GCC V3.4
-
-  -------------------------------------------------------------------------
-
-  Revision History:
-
-  2006/07/06 k.t.:   start of the implementation
-
-
-****************************************************************************/
-
-#ifndef _EPLTIMER_H_
-#define _EPLTIMER_H_
-
+//------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
 #include <oplk/oplkinc.h>
 #include <oplk/event.h>
 
-
-//---------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // const defines
-//---------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
-
-//---------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // typedef
-//---------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
 // type for timer handle
 #if (TARGET_SYSTEM == _WIN32_)
 typedef ULONG_PTR tEplTimerHdl;
-
 #else
-
-typedef unsigned long tEplTimerHdl;
+typedef ULONG tEplTimerHdl;
 #endif
 
 typedef struct
@@ -99,17 +64,9 @@ typedef struct
     tEplEventSink   m_EventSink;
     union
     {
-        DWORD           m_dwVal;
+        UINT32          m_dwVal;
         void*           m_pVal;
     } m_Arg;
-//    unsigned long   m_ulArg;    // d.k.: converted to unsigned long because
-                                // it is never accessed as a pointer by the
-                                // timer module and the data the
-                                // pointer points to is not saved in any way.
-                                // It is just a value. The user is responsible
-                                // to store the data statically and convert
-                                // the pointer between address spaces.
-
 } tEplTimerArg;
 
 typedef struct
@@ -117,25 +74,11 @@ typedef struct
     tEplTimerHdl    m_TimerHdl;
     union
     {
-        DWORD           m_dwVal;
+        UINT32          m_dwVal;
         void*           m_pVal;
     } m_Arg;
-//    unsigned long   m_ulArg;    // d.k.: converted to unsigned long because
-                                // it is never accessed as a pointer by the
-                                // timer module and the data the
-                                // pointer points to is not saved in any way.
-                                // It is just a value.
-
 } tEplTimerEventArg;
 
-typedef tEplKernel (PUBLIC * tEplTimerkCallback) (
-    tEplTimerEventArg* pEventArg_p);
+typedef tEplKernel (*tEplTimerkCallback) (tEplTimerEventArg* pEventArg_p);
 
-//---------------------------------------------------------------------------
-// function prototypes
-//---------------------------------------------------------------------------
-
-
-#endif  // #ifndef _EPLTIMER_H_
-
-
+#endif /* _INC_timer_H_ */
