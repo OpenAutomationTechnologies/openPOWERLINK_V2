4ff08963d907d2a662be145e7402f85abcdd2280
Joerg Zelenka <joerg.zelenka@br-automation.com>
2014-02-05 09:33:10 +0100

    [FEATURE] Rework Altera Nios II build system
    - Create stack libraries (liboplkxxx) for CN and MN platform
    - Board configuration selects the appropriate library (stack.sh)
    - Move common build scripts to tools/altera-nios2
    - Modified drivers and apps build
    
    Change-Id: I22e852322d9e2ab8f31fade176899700e2aca171
    Reviewed-on: http://brateggevoat1/gerrit/1917
    Tested-by: Jenkins <jenkins@brateggevoat1.br-automation.co.at>
    Reviewed-by: Joerg Zelenka <joerg.zelenka@br-automation.com>
    Reviewed-by: Josef Baumgartner <josef.baumgartner@br-automation.com>

---
+++ tools/altera-nios2/drivers.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+################################################################################
+#
+# Drivers generator script for Altera Nios II
+#
+# Copyright (c) 2014, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the copyright holders nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+################################################################################
+
+DRV_PATH=$1
+BOARD_PATH=$2
+CFLAGS="-Wall -Wextra -pedantic -std=c99"
+
+if [ -z "${OPLK_BASE_DIR}" ];
+then
+    # Find oplk root path
+    SCRIPTDIR=$(dirname $(readlink -f "${0}"))
+    OPLK_BASE_DIR=$(readlink -f "${SCRIPTDIR}/../..")
+fi
+
+# process arguments
+DEBUG=
+DEBUG_ARG=
+OUT_PATH=.
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        --debug)
+            DEBUG=1
+            DEBUG_ARG=$1
+            ;;
+        --out)
+            shift
+            OUT_PATH=$1
+            ;;
+        --help)
+            echo "$ drivers.sh [DRV] [BOARD] [OPTIONS]"
+            echo "DRV       ... Path to drivers project (drivers.settings)"
+            echo "BOARD     ... Path to hardware project (board.settings)"
+            echo "OPTIONS   ... :"
+            echo "              --debug ... Lib is generated with O0"
+            echo "              --out   ... Path to output directory"
+            exit 1
+            ;;
+        *)
+            ;;
+    esac
+    shift
+done
+
+if [ -z "${DRV_PATH}" ];
+then
+    echo "ERROR: No driver path is given!"
+fi
+
+if [ -z "${BOARD_PATH}" ];
+then
+    echo "ERROR: No board path is given!"
+fi
+
+# Let's source the board.settings (null.settings before)
+BOARD_SETTINGS_FILE=${BOARD_PATH}/board.settings
+CFG_DRV_CPU_NAME=
+if [ -f ${BOARD_SETTINGS_FILE} ]; then
+    source ${BOARD_SETTINGS_FILE}
+else
+    echo "ERROR: ${BOARD_SETTINGS_FILE} not found!"
+    exit 1
+fi
+
+if [ -z "${CFG_DRV_CPU_NAME}" ]; then
+    echo "ERROR: The board has no CPU processing drv!"
+    exit 1
+fi
+
+BSP_PATH=${OUT_PATH}/bsp-${CFG_DRV_CPU_NAME}
+
+BSP_GEN_ARGS="${CFG_DRV_BSP_TYPE} ${BSP_PATH} ${BOARD_PATH}/quartus \
+--set hal.enable_c_plus_plus false \
+--set hal.linker.enable_alt_load_copy_exceptions false \
+--set hal.enable_clean_exit false \
+--set hal.enable_exit false \
+--cpu-name ${CFG_DRV_CPU_NAME} \
+--set hal.sys_clk_timer ${CFG_DRV_SYS_TIMER_NAME} \
+--cmd add_section_mapping .tc_i_mem ${CFG_DRV_TCI_MEM_NAME} \
+"
+
+if [ -z "${DEBUG}" ];
+then
+    BSP_GEN_ARGS+="\
+    --set hal.stdout none --set hal.stderr none \
+    --set hal.make.bsp_cflags_optimization ${CFG_DRV_BSP_OPT_LEVEL} \
+    "
+else
+    BSP_GEN_ARGS+="--set hal.make.bsp_cflags_optimization -O0"
+    echo "INFO: Prepare BSP for debug."
+fi
+
+nios2-bsp ${BSP_GEN_ARGS}
+RET=$?
+
+if [ ${RET} -ne 0 ]; then
+    echo "ERROR: BSP generation returned with error ${RET}!"
+    exit ${RET}
+fi
+
+# Now fix the generated linker.x file...
+chmod +x ${OPLK_BASE_DIR}/tools/altera-nios2/fix-linkerx
+${OPLK_BASE_DIR}/tools/altera-nios2/fix-linkerx ${BSP_PATH}
+
+# Generate the library fitting to the board
+chmod +x ${OPLK_BASE_DIR}/tools/altera-nios2/stack.sh
+${OPLK_BASE_DIR}/tools/altera-nios2/stack.sh ${BSP_PATH} --out ${OUT_PATH} ${DEBUG_ARG}
+RET=$?
+
+if [ ${RET} -ne 0 ]; then
+    echo "ERROR: Library generation returned with error ${RET}!"
+    exit ${RET}
+fi
+
+# Let's source the drivers.settings
+CFG_DRV_ARGS=
+DRV_SETTINGS_FILE=${DRV_PATH}/drivers.settings
+if [ -f ${DRV_SETTINGS_FILE} ]; then
+    source ${DRV_SETTINGS_FILE}
+else
+    echo "ERROR: ${DRV_SETTINGS_FILE} not found!"
+    exit 1
+fi
+
+if [ -n "${DEBUG}" ];
+then
+    DRV_OPT_LEVEL=-O0
+fi
+
+DRV_GEN_ARGS="\
+--app-dir ${DRV_PATH} \
+--bsp-dir ${BSP_PATH} \
+--elf-name ${DRV_NAME}.elf \
+--src-files ${DRV_SOURCES} \
+--set APP_CFLAGS_OPTIMIZATION ${DRV_OPT_LEVEL} \
+--set CFLAGS=${CFLAGS} \
+--set OBJDUMP_INCLUDE_SOURCE 1 \
+--set CREATE_OBJDUMP 0 \
+--set QSYS_SUB_CPU ${CFG_DRV_PROC_NAME} \
+--set QUARTUS_PROJECT_DIR=${BOARD_PATH}/quartus \
+${CFG_DRV_ARGS} \
+"
+
+# Add includes
+for i in ${DRV_INCLUDES}
+do
+    DRV_GEN_ARGS+="--inc-dir ${i} "
+done
+
+# And add stack library
+LIB_STACK_DIR=$(find ${OUT_PATH} -type d -name "liboplk*")
+
+DRV_GEN_ARGS+="--use-lib-dir ${LIB_STACK_DIR} "
+
+nios2-app-generate-makefile ${DRV_GEN_ARGS}
+RET=$?
+
+if [ ${RET} -ne 0 ]; then
+    echo "ERROR: Application generation returned with error ${RET}!"
+    exit ${RET}
+fi
+
+chmod +x ${OPLK_BASE_DIR}/tools/altera-nios2/fix-app-makefile
+${OPLK_BASE_DIR}/tools/altera-nios2/fix-app-makefile ${DRV_PATH}/Makefile
+
+exit 0
