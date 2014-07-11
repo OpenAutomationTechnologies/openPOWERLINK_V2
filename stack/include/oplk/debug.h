81eb91b1fe16d79ece24eef36034ade91b33c37a
Wolfgang Seiss <wolfgang.seiss@br-automation.com>
2014-07-11 11:52:49 +0200

    [TASK] Cleanup debug.h
    Change-Id: I5aeb9b202a1f4afc70da29c528cce8002b716dc9

--- stack/include/oplk/debug.h
+++ stack/include/oplk/debug.h
@@ -75,22 +75,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define DEBUG_LVL_ERROR                 0x40000000
 #define DEBUG_LVL_ALWAYS                0x80000000
 
-//------------------------------------------------------------------------------
-// this macro defines a version string
-#define MAKE_VERSION_STRING(product, appname, verstr, author) \
-    "$prd$:" product ",$app$:" appname ",$ver$:" verstr ",$dat$:" __DATE__ ",$aut$:" author
-
-
-//------------------------------------------------------------------------------
-// this macro defines a build info string (e.g. for using in printf())
-#define DEBUG_MAKE_BUILD_INFO(prefix, product, prodid, descr, verstr, author) "\n" \
-    prefix "***************************************************\n" \
-    prefix "Project:   " product ", " prodid                  "\n" \
-    prefix "Descript.: " descr                                "\n" \
-    prefix "Author:    " author                               "\n" \
-    prefix "Date:      " __DATE__                             "\n" \
-    prefix "Version:   " verstr                               "\n" \
-    prefix "***************************************************\n\n"
 
 //---------------------------------------------------------------------------
 // The default debug-level is: ERROR and ALWAYS.
@@ -104,18 +88,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endif
 
 //---------------------------------------------------------------------------
-#if (DEV_SYSTEM == _DEV_WIN32_) && defined (TRACE_MSG)
-
-// For WIN32 the macro DEBUG_TRACE can be defined as function call TraceLvl()
-// or as macro TRACE().
-// Here the parameter 'lvl' can be used with more than one
-// debug-level (using OR).
-// Example: DEBUG_TRACE(DEBUG_LVL_30 | DEBUG_LVL_02, "Hello %d", bCount);
-
-#define DEBUG_TRACE(lvl,...)                TraceLvl((lvl), __VA_ARGS__)
-#define DEBUG_GLB_LVL                       debugLevel_g
-
-#else
 
 // At microcontrollers we do reduce the memory usage by deleting DEBUG_TRACE-lines
 // (compiler does delete the lines).
@@ -234,100 +206,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define DEBUG_LVL_EDRV_TRACE(...)                  TRACE(__VA_ARGS__)
 #else
 #define DEBUG_LVL_EDRV_TRACE(...)
-#endif
-
-#define DEBUG_TRACE(lvl,...)                       lvl##_TRACE(__VA_ARGS__)
-
-#endif
-
-//------------------------------------------------------------------------------
-// The macro DEBUG_DUMP_DATA() can be used with the same debug-levels to dump
-// out data bytes. Function DumpData() has to be included.
-// NOTE: DUMP_DATA has to be defined in project settings.
-#if (!defined (NDEBUG) && defined (DUMP_DATA)) || (DEV_SYSTEM == _DEV_WIN32_)
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-void DumpData(char* szStr_p, UINT8* pData_p, UINT16 size_p);
-
-#ifdef __cplusplus
-}
-#endif
-
-#define DEBUG_DUMP_DATA(lvl, str, ptr, siz) \
-    if ((DEBUG_GLB_LVL & (lvl)) == (lvl)) \
-        DumpData (str, (UINT8*)(ptr), (UINT16)(siz));
-#else
-#define DEBUG_DUMP_DATA(lvl, str, ptr, siz)
-#endif
-
-
-//------------------------------------------------------------------------------
-// The macro DEBUG_ASSERT() can be used to print out an error string if the
-// parametered expression does not result TRUE.
-// NOTE: If DEBUG_KEEP_ASSERT is defined, then DEBUG_ASSERT-line will not be
-//       deleted from compiler (in release version too).
-#if !defined (NDEBUG) || defined (DEBUG_KEEP_ASSERT)
-
-#if (DEV_SYSTEM == _DEV_WIN32_)
-// For WIN32 process will be killed after closing message box.
-#define DEBUG_ASSERT(expr, str) \
-    if (!(expr ) && ((DEBUG_GLB_LVL & DEBUG_LVL_ASSERT)!=0)) \
-    { \
-        MessageBox(NULL, "Assertion failed: line " __LINE__ " file " __FILE__ "\n    -> " str "\n"); \
-        ExitProcess(-1); \
-    }
-
-#define DEBUG_ASSERT1(expr, str, p1) \
-    if (!(expr ) && ((DEBUG_GLB_LVL & DEBUG_LVL_ASSERT)!=0)) \
-    { \
-        MessageBox(NULL, "Assertion failed: line " __LINE__ " file " __FILE__ "\n    -> " str "\n"); \
-        ExitProcess(-1); \
-    }
-
-#else
-
-// For microcontrollers process will be stopped using endless loop.
-#define DEBUG_ASSERT0(expr, str) \
-    if (!(expr )) \
-    { \
-        DEBUG_LVL_ASSERT_TRACE("Assertion failed: line %d file '%s'\n" \
-                               "    -> '%s'\n", __LINE__, __FILE__, str); \
-        while (1); \
-    }
-
-#define DEBUG_ASSERT1(expr, str, p1) \
-    if (!(expr )) \
-    { \
-    DEBUG_LVL_ASSERT_TRACE("Assertion failed: line %d file '%s'\n" \
-                           "    -> '%s'\n" \
-                           "    -> 0x%08lX\n", __LINE__, __FILE__, str, (DWORD) p1); \
-    while (1); \
-    }
-
-#endif
-
-#else
-
-#define DEBUG_ASSERT0(expr, str)
-#define DEBUG_ASSERT1(expr, str, p1)
-
-#endif
-
-
-//------------------------------------------------------------------------------
-// The macro DEBUG_ONLY() implements code, if NDEBUG is not defined.
-//------------------------------------------------------------------------------
-#if !defined (DEBUG_ONLY)
-
-#if !defined (NDEBUG)
-#define DEBUG_ONLY(expr)    expr
-#else
-#define DEBUG_ONLY(expr)
-#endif
-
 #endif
 
 
