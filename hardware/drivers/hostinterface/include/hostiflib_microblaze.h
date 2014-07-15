816a49cc6a141e4530d86ceec37f3eca94d728b4
Vinod PA <vinodpa@kalycito.com>
2014-07-15 15:43:24 +0530

    [FEATURE] Add Microblaze support in host interface driver
    - Implement target specific functions
    - Clean up files which have to be changed
    
    Signed-off-by: Joerg Zelenka <joerg.zelenka@br-automation.com>
    
    Change-Id: I1eab35b7496753e36700d32ab13e7e4d4aecd428

---
+++ hardware/drivers/hostinterface/include/hostiflib_microblaze.h
@@ -0,0 +1,121 @@
+/**
+********************************************************************************
+\file   hostiflib_microblaze.h
+
+\brief  Host Interface Library - For Microblaze target
+
+This header file provides specific macros for Xilinx Microblaze CPU.
+
+*******************************************************************************/
+/*------------------------------------------------------------------------------
+Copyright (c) 2014 Kalycito Infotech Private Limited
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
+#ifndef _INC_hostiflib_microblaze_H_
+#define _INC_hostiflib_microblaze_H_
+
+//------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
+#include <stddef.h>
+#include <xparameters.h>
+
+#include <cachemem/cachemem.h> // For data cache aware memory access
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+#if CONFIG_HOSTIF_PCP != FALSE
+
+#define HOSTIF_BASE             XPAR_AXI_HOSTINTERFACE_0_BASEADDR
+
+#else
+
+#ifdef XPAR_AXI_HOSTINTERFACE_0_HOST_BASEADDR
+
+#define HOSTIF_BASE             XPAR_AXI_HOSTINTERFACE_0_HOST_BASEADDR
+
+#ifdef XPAR_HOST_INTC_DEVICE_ID
+
+#define HOSTIF_IRQ_IC_ID        XPAR_HOST_INTC_BASEADDR
+#define HOSTIF_IRQ              XPAR_HOST_INTC_AXI_HOSTINTERFACE_0_IRQOUT_IRQ_INTR
+
+#else
+
+#error No Valid Interrupt Controller found for Host!
+
+#endif // XPAR_HOST_INTC_DEVICE_ID
+
+#else
+// Parallel Interface Host  External Processor
+#warning using external host. Ensure the interface base address is correct
+#define HOSTIF_BASE             XPAR_AXI_EPC_0_PRH0_BASEADDR
+
+#ifdef XPAR_HOST_INTC_DEVICE_ID
+
+#define HOSTIF_IRQ_IC_ID        XPAR_HOST_INTC_BASEADDR
+#define HOSTIF_IRQ              XPAR_HOST_INTC_SYSTEM_AXI_HOSTINTERFACE_0_IRQOUT_IRQ_PIN_0_INTR
+
+#else
+
+#error No Valid Interrupt Controller found for Host!
+
+#endif  // XPAR_HOST_INTC_DEVICE_ID
+
+#endif  // defined(XPAR_AXI_HOSTINTERFACE_0_HOST_BASEADDR)
+#endif  // CONFIG_HOSTIF_PCP != FALSE
+
+#if (XPAR_MICROBLAZE_USE_DCACHE == 1)
+#define HOSTIF_SYNC_DCACHE      TRUE
+#else
+#define HOSTIF_SYNC_DCACHE      FALSE
+#endif
+
+#define HOSTIF_MAKE_NONCACHEABLE(ptr)       (void*) ptr
+
+#define HOSTIF_UNCACHED_MALLOC(size)        malloc(size)
+#define HOSTIF_UNCACHED_FREE(ptr)           free(ptr)
+
+// sleep
+#define HOSTIF_USLEEP(x)                    usleep(x)
+
+// hw access
+#define HOSTIF_RD32(base, offset)           cachemem_invAndRead32(base, offset)
+#define HOSTIF_RD16(base, offset)           cachemem_invAndRead16(base, offset)
+#define HOSTIF_RD8(base, offset)            cachemem_invAndRead8(base, offset)
+
+#define HOSTIF_WR32(base, offset, dword)    cachemem_writeAndFlush32(base, offset, dword)
+#define HOSTIF_WR16(base, offset, word)     cachemem_writeAndFlush16(base, offset, word)
+#define HOSTIF_WR8(base, offset, byte)      cachemem_writeAndFlush8(base, offset, byte)
+
+//------------------------------------------------------------------------------
+// typedef
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// function prototypes
+//------------------------------------------------------------------------------
+
+#endif /* _INC_HOST_IF_MICROBLAZE_H_ */
---
+++ hardware/drivers/hostinterface/src/hostiflibint_microblaze.c
@@ -0,0 +1,153 @@
+/**
+********************************************************************************
+\file   hostiflibint_microblaze.c
+
+\brief  Host Interface Library - Driver Implementation for Microblaze
+
+The file contains the high level driver for the host interface library for
+Microblaze targets.
+
+\ingroup module_hostiflib
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+Copyright (c) 2014, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
+
+/**
+********************************************************************************
+\defgroup   module_hostiflib    Host Interface Library
+\ingroup    libraries
+
+The host interface library provides a software interface for using the host
+interface IP-Core. It provides several features like queues and linear memory
+modules.
+*******************************************************************************/
+
+//------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
+#include "hostiflib.h"
+#include "hostiflib_target.h"
+#include "hostiflibint.h"
+
+#include <xintc_l.h>
+#include <xil_io.h>
+#include <xparameters.h>
+
+//============================================================================//
+//            G L O B A L   D E F I N I T I O N S                             //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// module global vars
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// global function prototypes
+//------------------------------------------------------------------------------
+
+//============================================================================//
+//            P R I V A T E   D E F I N I T I O N S                           //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// local types
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// local vars
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// local function prototypes
+//------------------------------------------------------------------------------
+
+//============================================================================//
+//            P U B L I C   F U N C T I O N S                                 //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+/**
+\brief  Register interrupt service routine
+
+This function registers the interrupt service routine in the host processor
+interrupt services.
+
+\param  pfnIrqCb_p              The interrupt service routine callback
+\param  pArg_p                  Argument pointer provided to the callback
+
+\return The function always returns with kHostifSuccessful.
+
+\ingroup module_hostiflib
+*/
+//------------------------------------------------------------------------------
+tHostifReturn hostif_sysIrqRegHandler(tHostifIrqCb pfnIrqCb_p, void* pArg_p)
+{
+    XIntc_RegisterHandler(HOSTIF_IRQ_IC_ID, HOSTIF_IRQ, pfnIrqCb_p, pArg_p);
+
+    return kHostifSuccessful;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Enable interrupt for host interface driver
+
+This function enables the interrupt for the host interface driver instance.
+
+\param  fEnable_p   Determines if the interrupt must be enabled (TRUE) or
+                    disabled (FALSE)
+
+\return The function always returns with kHostifSuccessful.
+
+\ingroup module_hostiflib
+*/
+//------------------------------------------------------------------------------
+tHostifReturn hostif_sysIrqEnable(BOOL fEnable_p)
+{
+    if (fEnable_p)
+    {
+        XIntc_EnableIntr(HOSTIF_IRQ_IC_ID,
+                         (Xil_In32(HOSTIF_IRQ_IC_ID + XIN_IER_OFFSET) |
+                         (1 << HOSTIF_IRQ)));
+    }
+    else
+    {
+        XIntc_DisableIntr(HOSTIF_IRQ_IC_ID,
+                          (Xil_In32(HOSTIF_IRQ_IC_ID + XIN_IER_OFFSET) &
+                          ~(1 << HOSTIF_IRQ)))
+    }
+
+    return kHostifSuccessful;
+}
