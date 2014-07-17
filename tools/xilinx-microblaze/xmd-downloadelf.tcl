5ea6e49189d6ccaffa2f27eb5df79807c319776c
Vinod PA <vinodpa@kalycito.com>
2014-07-17 19:29:33 +0530

    [TASK] Update download-elf script to download two elf
    Update download-elf scripts to download host and pcp application
    for dual processor design (lx150t board)

--- tools/xilinx-microblaze/xmd-downloadelf.tcl
+++ tools/xilinx-microblaze/xmd-downloadelf.tcl
@@ -1,8 +1,10 @@
 ################################################################################
 #
-# Connects to mb, download the elf file and set Microblaze to run
+# Descripition:
+#   Connects to mb, download the elf file and set Microblaze to run
 #
 # Copyright (c) 2014, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+# Copyright (c) 2014, Kalycito Infotech Pvt Ltd.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -30,9 +32,57 @@
 
 if { $argc != 2 } {
     #Exit on error
-    puts "ERROR: Invalid download script parameters!"
-    exit 1;
+    puts "Warning: Too Many download script parameters!"
+
+    #exit 1;
+    if { $argc != 3 } {
+     puts "Error: Invalid download script parameters!"
+     exit 1;
+    }
 }
+
+if { $argc == 3 } {
+
+    set enVerify [lindex $argv 2]
+
+    # Establish connection to debug module
+    # 0- app 1- drv
+    connect mb mdm -debugdevice cpunr 1
+
+    # Read name of executeable from script arguments
+    puts "[lindex $argv 1] is downloading....."
+    set executeable "[lindex $argv 1]"
+    dow $executeable
+
+    if { $enVerify } {
+        puts "INFO: Verify elf download. This can take a few minutes!"
+        elf_verify
+    }
+    # Reset system before run
+    debugconfig -reset_on_run processor enable
+    run
+    disconnect 0
+
+    #############################################
+    # Establish connection to debug module
+    connect mb mdm -debugdevice cpunr 2
+
+    # Read name of executeable from script arguments
+    puts "[lindex $argv 0] is downloading....."
+    set executeable "[lindex $argv 0]"
+    dow $executeable
+
+    if { $enVerify } {
+        puts "INFO: Verify elf download. This can take a few minutes!"
+        elf_verify
+    }
+
+    # Reset system before run
+    debugconfig -reset_on_run processor enable
+    run
+    disconnect 1
+    exit;
+} else {
 
 set enVerify [lindex $argv 1]
 
@@ -55,3 +105,4 @@ run
 
 disconnect 0
 exit;
+}
