fe7fd58f4936782680d75568ac0b7331f7ff7c8c
Gaurav Kumar Singh <gaurav@kalycito.com>
2014-07-21 17:52:29 +0530

    [TASK] Update BSP and bitstream to support Zynq
    - Update BSP generation marco to add optional bootloader compilation.
    - Add installation steps for additional files required for Zynq PS
      initialization.

--- hardware/cmake/xilinx/installbitstream.cmake
+++ hardware/cmake/xilinx/installbitstream.cmake
@@ -3,6 +3,7 @@
 # CMake macro for installing the bitstream for Microblaze
 #
 # Copyright (c) 2014, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+# Copyright (c) 2014, Kalycito Infotech Private Limited
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -32,6 +33,13 @@ MACRO(INSTALL_BITSTREAM EXAMPLE_ROOT BITS_DESTINATION)
     SET(SDK_EXPORT ${EXAMPLE_ROOT}/xps/SDK/SDK_Export/hw)
     SET(DLCMD_SOURCE_DIR ${EXAMPLE_ROOT}/xps/etc)
 
+##########################################################
+# Additionally include the default XML path to avoid tool
+# related issues
+    IF(${CFG_DEMO_BOARD_NAME} STREQUAL "xilinx-z702")
+    SET(ZYNQ_XML ${EXAMPLE_ROOT}/sdk)
+    ENDIF(${CFG_DEMO_BOARD_NAME} STREQUAL "xilinx-z702")
+
     # Remove folder prefix from download.cmd script
     FILE(READ ${DLCMD_SOURCE_DIR}/download.cmd DLCMD_CONTENT)
     STRING(REGEX REPLACE "implementation/" ""
@@ -44,6 +52,12 @@ MACRO(INSTALL_BITSTREAM EXAMPLE_ROOT BITS_DESTINATION)
     INSTALL(FILES ${SDK_EXPORT}/system.bit ${SDK_EXPORT}/download.bit ${SDK_EXPORT}/system.xml ${SDK_EXPORT}/system_bd.bmm
             DESTINATION ${BITS_DESTINATION}
            )
+    # Additional initialization modules generated for ARM on Zynq
+    IF(${CFG_DEMO_BOARD_NAME} STREQUAL "xilinx-z702")
+        INSTALL(FILES ${SDK_EXPORT}/ps7_init.tcl ${SDK_EXPORT}/ps7_init.c ${SDK_EXPORT}/ps7_init.h ${ZYNQ_XML}/system.xml
+            DESTINATION ${BITS_DESTINATION}
+           )
+    ENDIF(${CFG_DEMO_BOARD_NAME} STREQUAL "xilinx-z702")
 
     INSTALL(FILES ${PROJECT_BINARY_DIR}/hwplatformproject.in
             DESTINATION ${BITS_DESTINATION} RENAME .project
