f62215b14999d0c8eb75dc85e215a00733a91485
Vinod PA <vinodpa@kalycito.com>
2014-07-21 17:54:33 +0530

    [FEATURE] Add Xilinx Zynq MN design for Z702 board
    This Xilinx Zynq MN design uses a Microblaze soft-core for the kernel
    and ARM core 0 for the user layer.
    
    - Add XPS project files
    - Add SDK support files for PS and Microblaze (PL)
    
    Note: The file in sdk/system.xml overwrites the XPS generated file to
          workaround a Xilinx tool bug (XPS 14.7).
    
    Change-Id: I1c6801573ce7aac275beff4bfef984f73be4cc7b
    Signed-off-by: Joerg Zelenka <joerg.zelenka@br-automation.com>

--- hardware/cmake/configure-zynqarm.cmake
+++ hardware/cmake/configure-zynqarm.cmake
@@ -1,8 +1,8 @@
 ################################################################################
 #
-# CMake boards configuration file for Microblaze platform
+# CMake boards configuration file for Xilinx Zynq ARM
 #
-# Copyright (c) 2014, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+# Copyright (c) 2014, Kalycito Infotech Private Limited
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -35,13 +35,13 @@ SET(CMAKE_MODULE_PATH "${OPLK_BASE_DIR}/cmake" ${CMAKE_MODULE_PATH})
 
 INCLUDE(geneclipsefilelist)
 INCLUDE(geneclipseincludelist)
-INCLUDE(setmicroblazeboardconfig)
+INCLUDE(setzynqarmboardconfig)
 
 ################################################################################
 # U S E R    O P T I O N S
 
 # Assemble path to all boards with Xilinx demos
-SET(BOARD_DIRS ${PROJECT_SOURCE_DIR}/boards/avnet-s6plkeb;${PROJECT_SOURCE_DIR}/boards/avnet-lx150t;${PROJECT_SOURCE_DIR}/boards/xilinx-z702)
+SET(BOARD_DIRS ${PROJECT_SOURCE_DIR}/boards/xilinx-z702)
 
 ################################################################################
 # Find the Xilinx toolchain
@@ -62,4 +62,4 @@ FIND_PROGRAM(XIL_XPS NAMES xps
 ################################################################################
 # Set path to system folders
 SET(ARCH_IPCORE_REPO ${PROJECT_SOURCE_DIR}/ipcore/xilinx)
-SET(ARCH_TOOLS_DIR ${OPLK_BASE_DIR}/tools/xilinx-microblaze)
+SET(ARCH_TOOLS_DIR ${OPLK_BASE_DIR}/tools/xilinx-zynqarm)
