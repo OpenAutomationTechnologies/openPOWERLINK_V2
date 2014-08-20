469aaf0f4e4b4d99a4999a4d80a8999c73c07e17
Joerg Zelenka <joerg.zelenka@br-automation.com>
2014-08-20 07:02:12 +0200

    [FIX] Fix clock crossing ipcore
    This commit solves clock transfer issues with commands in tight
    sequences. The fast and slow clock domain instances are redesigned using
    a state machine. The fast clock domain instance uses a dedicated signal
    for synchronizing the slow clock domain state machine.
    
    Change-Id: Id86edcd897c8dccd64957aaaa6ebe1658cc88373

--- hardware/ipcore/common/lib/src/clkXingRtl.vhd
+++ hardware/ipcore/common/lib/src/clkXingRtl.vhd
@@ -76,166 +76,173 @@ entity clkXing is
 end entity;
 
 architecture rtl of clkXing is
-    signal slowCs : std_logic_vector(gCsNum-1 downto 0);
-    signal anyCs : std_logic;
-    signal slowRnw : std_logic;
-    signal wr, wr_s, wr_rising : std_logic;
-    signal rd, rd_s, rd_rising : std_logic;
-    signal readRegister : std_logic_vector(gDataWidth-1 downto 0);
-    signal fastWrAck, fastRdAck, fastAnyAck, slowAnyAck : std_logic;
+    --! Fsm type
+    type tFsm is (sIdle, sTransfer);
+    --! Clock domain registers
+    type tRegClkDomain is record
+        chipselect      : std_logic_vector(gCsNum-1 downto 0);
+        rnw             : std_logic;
+        sync            : std_logic;
+        ack             : std_logic;
+        fsm             : tFsm;
+    end record;
+    --! Register init
+    constant cRegClkDomainInit : tRegClkDomain := (
+        chipselect      => (others => cInactivated),
+        rnw             => cInactivated,
+        sync            => cInactivated,
+        ack             => cInactivated,
+        fsm             => sIdle
+    );
+
+    --! Slow clock domain register
+    signal slowClkReg       : tRegClkDomain;
+    signal slowClkReg_next  : tRegClkDomain;
+
+    --! Fast clock domain register
+    signal fastClkReg       : tRegClkDomain;
+    signal fastClkReg_next  : tRegClkDomain;
+
+    --! Readdata register
+    signal readdataReg      : std_logic_vector(iSlowReaddata'range);
+    signal readdataReg_next : std_logic_vector(readdataReg'range);
+
+    --! Transferred sync signal (fast ---> slow)
+    signal tranSync_sync    : std_logic;
+    --! Transferred ack signal (slow ---> fast)
+    signal tranAck_sync     : std_logic;
 begin
-    -- WELCOME TO SLOW CLOCK DOMAIN --
-    genThoseCs : for i in slowCs'range generate
+    -- Map registers to outputs
+    -- FAST
+    oFastWrAck      <= fastClkReg.ack and not iFastRNW;
+    oFastRdAck      <= fastClkReg.ack and iFastRNW;
+    oFastReaddata   <= readdataReg;
+    -- SLOW
+    oSlowCs         <= slowClkReg.chipselect;
+    oSlowRNW        <= iFastRNW;
+
+    --! Fast clock domain logic
+    fastComb : process (
+        fastClkReg,
+        tranAck_sync,
+        iFastCs, iFastRNW
+    )
     begin
-        theSyncCs : entity libcommon.synchronizer
-            generic map (
-                gStages => 2,
-                gInit   => cInactivated
-            )
-            port map (
-                iArst   => iArst,
-                iClk    => iSlowClk,
-                iAsync  => iFastCs(i),
-                oSync   => slowCs(i)
-            );
-    end generate;
+        -- General defaults to avoid latches
+        fastClkReg_next <= fastClkReg;
 
-    anyCs <= OR_REDUCE(slowCs);
+        -- Inactivate pulsing registers
+        fastClkReg_next.sync <= cInactivated;
 
-    wr_s <= anyCs and not(slowRnw) and not slowAnyAck;
-    rd_s <= anyCs and slowRnw and not slowAnyAck;
+        -- Assign transferred signals
+        fastClkReg_next.ack <= tranAck_sync;
 
-    process(iArst, iSlowClk)
+        case fastClkReg.fsm is
+            when sIdle =>
+                if OR_REDUCE(iFastCs) = cActivated then
+                    fastClkReg_next.fsm         <= sTransfer;
+                    fastClkReg_next.sync        <= cActivated;
+                    fastClkReg_next.chipselect  <= iFastCs;
+                    fastClkReg_next.rnw         <= iFastRNW;
+                end if;
+
+            when sTransfer =>
+                if fastClkReg.ack = cActivated then
+                    fastClkReg_next.fsm <= sIdle;
+                end if;
+
+        end case;
+    end process fastComb;
+
+    --! Slow clock domain logic
+    slowComb : process (
+        slowClkReg, fastClkReg, readdataReg,
+        tranSync_sync,
+        iSlowWrAck, iSlowRdAck, iSlowReaddata
+    )
     begin
-        if iArst = '1' then
-            readRegister <= (others => '0');
-            wr <= '0';
-            rd <= '0';
-        elsif rising_edge(iSlowClk) then
-            if rd = '1' and iSlowRdAck = '1' then
-                readRegister <= iSlowReaddata;
-            end if;
+        -- Default
+        slowClkReg_next     <= slowClkReg;
+        readdataReg_next    <= readdataReg;
 
-            if iSlowWrAck = '1' then
-                wr <= '0';
-            elsif wr_rising = '1' then
-                wr <= '1';
-            end if;
+        -- Inactivate pulsing registers
+        slowClkReg_next.ack <= cInactivated;
 
-            if iSlowRdAck = '1' then
-                rd <= '0';
-            elsif rd_rising = '1' then
-                rd <= '1';
-            end if;
+        -- Assign transferred signals
+        slowClkReg_next.sync <= tranSync_sync;
+
+        case slowClkReg.fsm is
+            when sIdle =>
+                if slowClkReg.sync = cActivated then
+                    slowClkReg_next.fsm <= sTransfer;
+                end if;
+
+            when sTransfer =>
+                slowClkReg_next.chipselect  <= fastClkReg.chipselect;
+                slowClkReg_next.rnw         <= fastClkReg.rnw;
+
+                if iSlowRdAck = cActivated or iSlowWrAck = cActivated then
+                    slowClkReg_next.fsm         <= sIdle;
+                    slowClkReg_next.chipselect  <= (others => cInactivated);
+                    slowClkReg_next.ack         <= cActivated;
+
+                    if iSlowRdAck = cActivated then
+                        readdataReg_next <= iSlowReaddata;
+                    end if;
+                end if;
+
+        end case;
+    end process slowComb;
+
+    --! Fast clock registers
+    fastClockReg : process(iArst, iFastClk)
+    begin
+        if iArst = cActivated then
+            fastClkReg  <= cRegClkDomainInit;
+        elsif rising_edge(iFastClk) then
+            fastClkReg  <= fastClkReg_next;
         end if;
-    end process;
+    end process fastClockReg;
 
-    oSlowCs <= slowCs when wr = '1' or rd = '1' else (others => '0');
-    oSlowRNW <= rd;
+    --! Slow clock registers
+    slowClockReg : process(iArst, iSlowClk)
+    begin
+        if iArst = cActivated then
+            slowClkReg  <= cRegClkDomainInit;
+            readdataReg <= (others => cInactivated);
+        elsif rising_edge(iSlowClk) then
+            slowClkReg  <= slowClkReg_next;
+            readdataReg <= readdataReg_next;
+        end if;
+    end process slowClockReg;
 
-    theWriteEdge : entity libcommon.edgedetector
-        port map (
-            iArst       => iArst,
-            iClk        => iSlowClk,
-            iEnable     => cActivated,
-            iData       => wr_s,
-            oRising     => wr_rising,
-            oFalling    => open,
-            oAny        => open
-        );
-
-    theReadEdge : entity libcommon.edgedetector
-        port map (
-            iArst       => iArst,
-            iClk        => iSlowClk,
-            iEnable     => cActivated,
-            iData       => rd_s,
-            oRising     => rd_rising,
-            oFalling    => open,
-            oAny        => open
-        );
-
-    theSyncRnw : entity libcommon.synchronizer
+    --! Transfer sync pulse to slow clock domain
+    tranSyncToSlow : entity libcommon.syncTog
         generic map (
-            gStages => 2,
-            gInit   => cInactivated
-        )
-        port map (
-            iArst   => iArst,
-            iClk    => iSlowClk,
-            iAsync  => iFastRNW,
-            oSync   => slowRnw
-        );
-
-    theSyncAnyAck : entity libcommon.syncTog
-        generic map (
-            gStages => 2,
-            gInit   => cInactivated
+            gStages     => 2,
+            gInit       => cInactivated
         )
         port map (
             iSrc_rst    => iArst,
             iSrc_clk    => iFastClk,
-            iSrc_data   => fastAnyAck,
+            iSrc_data   => fastClkReg.sync,
             iDst_rst    => iArst,
             iDst_clk    => iSlowClk,
-            oDst_data   => slowAnyAck
+            oDst_data   => tranSync_sync
         );
 
-    -- WELCOME TO FAST CLOCK DOMAIN --
-
-    process(iArst, iFastClk)
-    begin
-        if iArst = '1' then
-            fastAnyAck <= '0';
-        elsif rising_edge(iFastClk) then
-            fastAnyAck <= fastWrAck or fastRdAck;
-        end if;
-    end process;
-
-    theSyncWrAck : entity libcommon.syncTog
+    --! Transfer ack pulse to fast clock domain
+    tranAckToFast : entity libcommon.syncTog
         generic map (
-            gStages => 2,
-            gInit   => cInactivated
+            gStages     => 2,
+            gInit       => cInactivated
         )
         port map (
             iSrc_rst    => iArst,
             iSrc_clk    => iSlowClk,
-            iSrc_data   => iSlowWrAck,
+            iSrc_data   => slowClkReg.ack,
             iDst_rst    => iArst,
             iDst_clk    => iFastClk,
-            oDst_data   => fastWrAck
+            oDst_data   => tranAck_sync
         );
-
-    oFastWrAck <= fastWrAck;
-
-    theSyncRdAck : entity libcommon.syncTog
-        generic map (
-            gStages => 2,
-            gInit   => cInactivated
-        )
-        port map (
-            iSrc_rst    => iArst,
-            iSrc_clk    => iSlowClk,
-            iSrc_data   => iSlowRdAck,
-            iDst_rst    => iArst,
-            iDst_clk    => iFastClk,
-            oDst_data   => fastRdAck
-        );
-
-    oFastRdAck <= fastRdAck;
-
-    genThoseRdq : for i in readRegister'range generate
-    begin
-        theSyncRdq : entity libcommon.synchronizer
-            generic map (
-                gStages => 2,
-                gInit   => cInactivated
-            )
-            port map (
-                iArst   => iArst,
-                iClk    => iFastClk,
-                iAsync  => readRegister(i),
-                oSync   => oFastReaddata(i)
-            );
-    end generate;
 end architecture;
