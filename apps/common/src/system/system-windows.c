ea6a07c1ad0ce4b825f4e9e3daf79f340c8312e3
Gaurav Kumar Singh <gaurav@kalycito.com>
2015-04-09 18:45:49 +0530

    [FIX] Fix application hangs with Windows kernel drivers
    Change-Id: Ia341f12efffabf3a28081d3b14ab160ad7a5f1e1
    Signed-off-by: Gaurav Kumar Singh <gaurav@kalycito.com>
    Signed-off-by: Joerg Zelenka <joerg.zelenka@br-automation.com>

--- apps/common/src/system/system-windows.c
+++ apps/common/src/system/system-windows.c
@@ -226,7 +226,7 @@ void system_stopSyncThread(void)
 
     // Signal to stop the thread and wait for the thread to terminate
     syncThreadInstance_l.fThreadExit = TRUE;
-    WaitForSingleObject(syncThreadInstance_l.hSyncThreadHandle, INFINITE);
+    WaitForSingleObject(syncThreadInstance_l.hSyncThreadHandle, 1000);
 }
 #endif
 
