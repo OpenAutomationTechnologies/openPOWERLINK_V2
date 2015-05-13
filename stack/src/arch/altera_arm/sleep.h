4d0aa1e620c4f5e19c275d9a75516861c46fc1bd
John <chidrupaya.sr@kalycito.com>
2015-05-13 18:41:40 +0530

    [TASK] Add support in stack, for Altera ARM processor
    - Add architecture specific sources for Altera ARM based designs.
    
    Change-Id: I1a97ba332e8e76feb701e5a2d8896de4c23116a5

---
+++ stack/src/arch/altera_arm/sleep.h
@@ -0,0 +1,68 @@
+/**
+********************************************************************************
+\file   altera_arm/sleep.h
+
+\brief  Inexact sleep implementation for Altera Cyclone V ARM
+
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+Copyright (c) 2015, Kalycito Infotech Private Limited
+Copyright (c) 2012, SYSTEC electronic GmbH
+Copyright (c) 2014, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
+
+#ifndef _INC_altera_arm_sleep_H_
+#define _INC_altera_arm_sleep_H_
+
+//------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// typedef
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// function prototypes
+//------------------------------------------------------------------------------
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int usleep(unsigned long int usecs_p);
+int msleep(unsigned long int msecs_p) __attribute__((optimize(0)));
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _INC_altera_arm_sleep_H_ */
---
+++ stack/src/arch/altera_arm/target-mutex.c
@@ -0,0 +1,170 @@
+/**
+********************************************************************************
+\file   target-mutex.c
+
+\brief  Architecture specific mutex implementation
+
+This file contains the mutex implementation for Altera ARM.
+
+\note As there is no mutli-threading environment on ARM, the functions are
+only dummy functions.
+
+\ingroup module_target
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+Copyright (c) 2014, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
+
+//------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
+#include <common/oplkinc.h>
+
+//============================================================================//
+//            G L O B A L   D E F I N I T I O N S                             //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// module global vars
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// global function prototypes
+//------------------------------------------------------------------------------
+
+//============================================================================//
+//            P R I V A T E   D E F I N I T I O N S                           //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// local types
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// local vars
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// local function prototypes
+//------------------------------------------------------------------------------
+
+//============================================================================//
+//            P U B L I C   F U N C T I O N S                                 //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+/**
+\brief  Create Mutex
+
+The function creates a mutex.
+
+\param  mutexName_p             The name of the mutex to create.
+\param  pMutex_p                Pointer to store the created mutex.
+
+\return The function returns a tOplkError error code.
+\retval kErrorOk                Mutex was successfully created.
+\retval kErrorNoFreeInstance    An error occured while creating the mutex.
+
+\ingroup module_target
+*/
+//------------------------------------------------------------------------------
+tOplkError target_createMutex(char* mutexName_p, OPLK_MUTEX_T* pMutex_p)
+{
+    UNUSED_PARAMETER(mutexName_p);
+    UNUSED_PARAMETER(pMutex_p);
+
+    return kErrorOk;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Destroy Mutex
+
+The function destroys a mutex.
+
+\param  mutexId_p               The ID of the mutex to destroy.
+
+\ingroup module_target
+*/
+//------------------------------------------------------------------------------
+void target_destroyMutex(OPLK_MUTEX_T mutexId_p)
+{
+    UNUSED_PARAMETER(mutexId_p);
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Lock Mutex
+
+The function locks a mutex.
+
+\param  mutexId_p               The ID of the mutex to lock.
+
+\return The function returns a tOplkError error code.
+\retval kErrorOk                Mutex was successfully locked.
+\retval kErrorNoFreeInstance    An error occured while locking the mutex.
+
+\ingroup module_target
+*/
+//------------------------------------------------------------------------------
+tOplkError target_lockMutex(OPLK_MUTEX_T mutexId_p)
+{
+    UNUSED_PARAMETER(mutexId_p);
+    return kErrorOk;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Unlock Mutex
+
+The function unlocks a mutex.
+
+\param  mutexId_p               The ID of the mutex to unlock.
+
+\ingroup module_target
+*/
+//------------------------------------------------------------------------------
+void target_unlockMutex(OPLK_MUTEX_T mutexId_p)
+{
+    UNUSED_PARAMETER(mutexId_p);
+}
+
+//============================================================================//
+//            P R I V A T E   F U N C T I O N S                               //
+//============================================================================//
+/// \name Private Functions
+/// \{
+
+///\}
