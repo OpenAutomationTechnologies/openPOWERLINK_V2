00309e5f7ee7753ef3310b934fc0c6e89b08fb3e
John <chidrupaya.sr@kalycito.com>
2015-05-15 04:34:34 +0530

    [TASK] Add CMake build support in stack library for Altera ARM
    Add CMake build support for stack library generation for Altera ARM using
    dual processor shared memory interface.
    
    Change-Id: I14dc658b23f9d6557247dd778c7c3273969c7d18

---
+++ stack/cmake/options-alterac5arm.cmake
@@ -0,0 +1,60 @@
+################################################################################
+#
+# CMake options for openPOWERLINK stack on Altera Cyclone V ARM
+#
+# Copyright (c) 2014, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+# Copyright (c) 2014, Kalycito Infotech Private Limited
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the copyright holders nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+################################################################################
+
+MESSAGE(STATUS "Adding CMake configuration options for ARM")
+
+################################################################################
+# Handle includes
+SET(CMAKE_MODULE_PATH "${OPLK_BASE_DIR}/cmake" ${CMAKE_MODULE_PATH})
+
+################################################################################
+# Set Paths
+SET(ALT_HW_LIB_DIR ${OPLK_BASE_DIR}/hardware/lib/${SYSTEM_NAME_DIR}/${SYSTEM_PROCESSOR_DIR})
+SET(ALT_TOOLS_DIR ${TOOLS_DIR}/altera-arm)
+
+################################################################################
+# Options for MN libraries
+OPTION(CFG_COMPILE_LIB_MNAPP_DUALPROCSHM "Compile openPOWERLINK MN library for application using dual processor shared memory" OFF)
+
+################################################################################
+# Add library subdirectories and hardware library path
+
+# MN libraries
+IF(CFG_COMPILE_LIB_MNAPP_DUALPROCSHM)
+      # Path to the hardware library folder of your board example
+      SET(CFG_COMPILE_LIB_MN_HW_LIB_DIR ${ALT_HW_LIB_DIR}/altera-c5soc/mn-soc-shmem-gpio
+                CACHE PATH "Path to the hardware library folder for the dual processor MN library")
+
+    ADD_SUBDIRECTORY(proj/generic/liboplkmnapp-dualprocshm)
+
+ELSE ()
+    UNSET(CFG_COMPILE_LIB_MN_HW_LIB_DIR CACHE)
+ENDIF ()
---
+++ stack/proj/generic/liboplkmnapp-dualprocshm/configure-c5socarm.cmake
@@ -0,0 +1,102 @@
+################################################################################
+#
+# CMake configuration for openPOWERLINK MN application library on Altera ARM
+#
+# Copyright (c) 2015, Kalycito Infotech Private Limited.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the copyright holders nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+################################################################################
+
+################################################################################
+# Include board specific settings file
+INCLUDE(setalteraarmboardconfig)
+
+SET_BOARD_CONFIGURATION(${CFG_COMPILE_LIB_MN_HW_LIB_DIR})
+
+################################################################################
+# Set paths
+SET(ARCH_INSTALL_POSTFIX ${CFG_DEMO_BOARD_NAME}/${CFG_DEMO_NAME})
+
+################################################################################
+# Find boards support package
+SET(ALT_BSP_DIR ${CFG_COMPILE_LIB_MN_HW_LIB_DIR}/bsp${CFG_HOST_NAME}/${CFG_HOST_NAME})
+
+MESSAGE(STATUS "Searching for the board support package in ${ALT_BSP_DIR}")
+IF(EXISTS ${ALT_BSP_DIR})
+    SET(ALT_LIB_BSP_INC ${ALT_BSP_DIR}/include)
+ELSE()
+    MESSAGE(FATAL_ERROR "Board support package for board ${CFG_DEMO_BOARD_NAME} and demo ${CFG_DEMO_NAME} not found!")
+ENDIF()
+
+################################################################################
+# Set architecture specific sources
+SET(LIB_ARCH_SOURCES
+                     ${TARGET_ALTERA_ARM_SOURCES}
+                     ${TARGET_ALTERA_ARM_DUAL_SOURCES}
+   )
+
+################################################################################
+# Deactivate optimization for usleep
+SET_SOURCE_FILES_PROPERTIES(${ARCH_SOURCE_DIR}/altera_arm/sleep.c
+                            PROPERTIES COMPILE_FLAGS "-O0")
+
+################################################################################
+# Set architecture specific includes
+INCLUDE_DIRECTORIES(
+                    ${ALT_LIB_BSP_INC}
+                    ${ARCH_SOURCE_DIR}/altera_arm
+                    ${CONTRIB_SOURCE_DIR}/dualprocshm/include
+                    ${CFG_COMPILE_LIB_MN_HW_LIB_DIR}/include
+                   )
+
+################################################################################
+# Set additional target specific compile flags
+ADD_DEFINITIONS("${ALT_HOST_CFLAGS} -fmessage-length=0 -ffunction-sections -fdata-sections -fno-inline")
+
+ADD_DEFINITIONS(-D__C5SOC__ -D__altera_arm__)
+################################################################################
+
+
+########################################################################
+# Eclipse project files
+GEN_ECLIPSE_FILE_LIST("${LIB_KERNEL_SOURCES}" "kernel" PART_ECLIPSE_FILE_LIST)
+SET(ECLIPSE_FILE_LIST "${ECLIPSE_FILE_LIST} ${PART_ECLIPSE_FILE_LIST}")
+
+GEN_ECLIPSE_FILE_LIST("${LIB_USER_SOURCES}" "user" PART_ECLIPSE_FILE_LIST)
+SET(ECLIPSE_FILE_LIST "${ECLIPSE_FILE_LIST} ${PART_ECLIPSE_FILE_LIST}")
+
+GEN_ECLIPSE_FILE_LIST("${LIB_COMMON_SOURCES}" "common" PART_ECLIPSE_FILE_LIST)
+SET(ECLIPSE_FILE_LIST "${ECLIPSE_FILE_LIST} ${PART_ECLIPSE_FILE_LIST}")
+
+GEN_ECLIPSE_FILE_LIST("${LIB_ARCH_SOURCES}" "arch" PART_ECLIPSE_FILE_LIST)
+SET(ECLIPSE_FILE_LIST "${ECLIPSE_FILE_LIST} ${PART_ECLIPSE_FILE_LIST}")
+
+GET_PROPERTY(LIBRARY_INCLUDES DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY INCLUDE_DIRECTORIES)
+GEN_ECLIPSE_INCLUDE_LIST("${LIBRARY_INCLUDES}" ECLIPSE_INCLUDE_LIST)
+
+GET_PROPERTY(FLAG_LIST DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY COMPILE_DEFINITIONS)
+GEN_ECLIPSE_FLAG_LIST("${FLAG_LIST}" ECLIPSE_FLAG_LIST)
+
+CONFIGURE_FILE(${ALT_TOOLS_DIR}/eclipse/libproject.in ${PROJECT_BINARY_DIR}/.project @ONLY)
+CONFIGURE_FILE(${ALT_TOOLS_DIR}/eclipse/libcproject.in ${PROJECT_BINARY_DIR}/.cproject @ONLY)
