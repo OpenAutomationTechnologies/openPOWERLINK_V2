1b36127aae539a00b485d5216a8e6891ac3f8f05
John <chidrupaya.sr@kalycito.com>
2015-05-21 17:24:23 +0530

    [TASK] Add support in stack for Altera ARM processor
    - Add architecture specific sources for Altera ARM based designs.
    
    Change-Id: I1a97ba332e8e76feb701e5a2d8896de4c23116a5

---
+++ stack/src/arch/altera-c5socarm/sleep.c
@@ -0,0 +1,264 @@
+/**
+********************************************************************************
+\file   altera-c5socarm/sleep.c
+
+\brief  Inexact usleep implementation for Altera Cyclone V ARM
+
+Waits an amount of microseconds or milliseconds. Uses global 64 bit timer ticks
+to check the time.
+
+\ingroup module_target
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+Copyright (c) 2015, Kalycito Infotech Private Limited
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
+
+//------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
+
+#include <sys/unistd.h>
+#include <alt_timers.h>
+#include <alt_globaltmr.h>
+#include <alt_interrupt.h>
+#include <alt_cache.h>
+#include <alt_fpga_manager.h>
+#include <alt_bridge_manager.h>
+#include <alt_address_space.h>
+#include <alt_mpu_registers.h>
+#include <alt_clock_manager.h>
+
+#include <system.h>
+
+#include <oplk/oplk.h>
+#include <oplk/debug.h>
+#include "sleep.h"
+
+//============================================================================//
+//            G L O B A L   D E F I N I T I O N S                             //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// module global vars
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// global function prototypes
+//------------------------------------------------------------------------------
+
+//============================================================================//
+//            P R I V A T E   D E F I N I T I O N S                           //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+
+#define SECS_TO_MILLISECS    1000
+
+//------------------------------------------------------------------------------
+// local types
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// local vars
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// local function prototypes
+//------------------------------------------------------------------------------
+
+static inline UINT64 getTimerTicksFromScaled(ALT_GPT_TIMER_t timerId_p,
+                                             UINT32 scalingFactor_p,
+                                             UINT32 scaledTimeDuration_p);
+
+//============================================================================//
+//            P U B L I C   F U N C T I O N S                                 //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+/**
+\brief    Sleep until limit of microseconds is reached
+
+\param  usecs_p                Count of microseconds to sleep
+
+\ingroup module_target
+*/
+//------------------------------------------------------------------------------
+INT usleep(ULONG usecs_p)
+{
+    UINT64          startTime = alt_globaltmr_get64();
+    UINT32          timerPrescaler = alt_globaltmr_prescaler_get() + 1;
+    UINT64          endTime;
+    alt_freq_t      timerClkSrc;
+
+    alt_clk_freq_get(ALT_CLK_MPU_PERIPH, &timerClkSrc);
+    endTime = startTime + usecs_p * ((timerClkSrc / timerPrescaler) / 1000000);
+
+    while (alt_globaltmr_get64() < endTime);
+    return 0;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief Sleep for the specified number of milliseconds
+
+The function makes the calling thread sleep until the number of specified
+milliseconds have elapsed.
+
+\param  milliSeconds_p      Number of milliseconds to sleep
+
+\ingroup module_target
+*/
+//------------------------------------------------------------------------------
+INT msleep(ULONG milliSeconds_p)
+{
+    UINT64                  startTickStamp = alt_globaltmr_get64();
+    UINT64                  waitTickCount = getTimerTicksFromScaled(ALT_GPT_CPU_GLOBAL_TMR, SECS_TO_MILLISECS, milliSeconds_p);
+    volatile UINT32*        pGlbTimerRegCntBaseLow = (volatile UINT32*)(GLOBALTMR_BASE + GLOBALTMR_CNTR_LO_REG_OFFSET);
+    volatile UINT32*        pGlbTimerRegCntBaseHigh = (volatile UINT32*)(GLOBALTMR_BASE + GLOBALTMR_CNTR_HI_REG_OFFSET);
+    UINT64                  curTickStamp = 0;
+    UINT32                  temp = 0;
+    UINT32                  hi = 0;
+    UINT32                  lo = 0;
+    volatile UINT32         waitCount = 0;
+    UINT32                  waitCountLimit = (UINT32)(5 + (waitTickCount >> 12));
+    UINT8                   fExit = FALSE;
+    UINT8                   readCntLimit = 3;
+
+    curTickStamp = alt_globaltmr_get64();
+
+    while (fExit != TRUE)
+    {
+        fExit = FALSE;
+
+        if (waitCount == waitCountLimit)
+        {
+            readCntLimit = 3;
+
+            do
+            {
+                temp = *pGlbTimerRegCntBaseHigh;
+                lo =  *pGlbTimerRegCntBaseLow;
+                hi = *pGlbTimerRegCntBaseHigh;
+            } while ((temp != hi) && (--readCntLimit));
+
+            if (readCntLimit != 0)
+            {
+                curTickStamp =  (UINT64)hi;
+                curTickStamp =  (((curTickStamp << 32) & ~((UINT64)UINT32_MAX)) | lo);
+                if ((((curTickStamp >= startTickStamp) && ((curTickStamp - startTickStamp) < waitTickCount)) ||
+                     ((curTickStamp < startTickStamp) && ((UINT64_MAX - startTickStamp) +
+                                                          curTickStamp) < waitTickCount)))
+                {
+                    fExit = FALSE;
+                }
+                else
+                {
+                    fExit = TRUE;
+                }
+
+                waitCount = 1;
+            }
+            else
+            {
+                waitCount--;
+            }
+        }
+
+        waitCount++;
+    }
+
+    return 0;
+}
+
+//============================================================================//
+//            P R I V A T E   F U N C T I O N S                               //
+//============================================================================//
+
+/// \name Private Functions
+/// \{
+//------------------------------------------------------------------------------
+/**
+\brief Convert time units into timer ticks
+
+The function converts the time in standard unit into ticks for the
+given timer.
+
+\param  timerId_p               The ALT_GPT_TIMER_t enum Id of the timer used
+\param  scalingFactor_p         Ratio of provided time duration scale to seconds
+\param  scaledTimeDuration_p    Time duration in standard unit to be converted
+
+\return The function returns a unsigned 64 bit value.
+\retval The converted tick count for the given timer.
+*/
+//------------------------------------------------------------------------------
+static inline UINT64 getTimerTicksFromScaled(ALT_GPT_TIMER_t timerId_p,
+                                             UINT32 scalingFactor_p,
+                                             UINT32 scaledTimeDuration_p)
+{
+    UINT64          ticks = 0;                      // value to return
+    ALT_CLK_t       clkSrc = ALT_CLK_UNKNOWN;
+    UINT32          preScaler = 0;
+    UINT32          freq = 1;
+
+    preScaler = alt_gpt_prescaler_get(timerId_p);
+    if (preScaler <= UINT8_MAX)
+    {
+        if (timerId_p == ALT_GPT_CPU_GLOBAL_TMR)    // Global Timer
+        {
+            ticks = 1;
+            clkSrc = ALT_CLK_MPU_PERIPH;
+        }
+        else
+        {
+            ticks = 0;
+            goto Exit;
+        }
+
+        if (alt_clk_freq_get(clkSrc, &freq) == ALT_E_SUCCESS)
+        {
+            // clock ticks per second
+            ticks *= freq;
+
+            // total clock ticks
+            ticks *= (UINT64)(scaledTimeDuration_p / scalingFactor_p);
+
+            // convert into timer ticks
+            ticks /= (preScaler + 1);
+        }
+    }
+
+Exit:
+    return ticks;
+}
+
+///\}
---
+++ stack/src/arch/altera-c5socarm/target-c5socarm.c
@@ -0,0 +1,499 @@
+/**
+********************************************************************************
+\file   altera-c5socarm/target-c5socarm.c
+
+\brief  Target specific functions for ARM on Altera SoC without OS
+
+This target depending module provides several functions that are necessary for
+Altera SoC ARM without OS.
+
+\ingroup module_target
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+Copyright (c) 2015, Kalycito Infotech Private Limited
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
+
+//------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
+
+#include <sys/unistd.h>
+#include <alt_timers.h>
+#include <alt_globaltmr.h>
+#include <alt_interrupt.h>
+#include <alt_cache.h>
+#include <alt_fpga_manager.h>
+#include <alt_bridge_manager.h>
+#include <alt_address_space.h>
+#include <alt_mpu_registers.h>
+#include <alt_clock_manager.h>
+
+#include <system.h>
+
+#include <oplk/oplk.h>
+#include <oplk/debug.h>
+#include <common/target.h>
+#include "sleep.h"
+//============================================================================//
+//            G L O B A L   D E F I N I T I O N S                             //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// module global vars
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// global function prototypes
+//------------------------------------------------------------------------------
+
+//============================================================================//
+//            P R I V A T E   D E F I N I T I O N S                           //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+#define SECS_TO_MILLISECS    1000
+
+//------------------------------------------------------------------------------
+// local types
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// external vars
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// local vars
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// local function prototypes
+//------------------------------------------------------------------------------
+
+static inline INT       enableInterruptMaster(void);
+static inline INT       disableInterruptMaster(void);
+
+static inline UINT64    getTimerMaxScaledCount(ALT_GPT_TIMER_t timerId_p,
+                                               UINT32 scalingFactor_p);
+static inline UINT64    getTimerCurrentScaledCount(ALT_GPT_TIMER_t timerId_p,
+                                                   UINT32 scalingFactor_p);
+
+//============================================================================//
+//            P U B L I C   F U N C T I O N S                                 //
+//============================================================================//
+
+//------------------------------------------------------------------------------
+/**
+\brief    Get current system tick
+
+This function returns the current system tick determined by the system timer.
+
+\return The function returns the system tick in milliseconds
+
+\ingroup module_target
+*/
+//------------------------------------------------------------------------------
+UINT32 target_getTickCount(void)
+{
+    return (UINT32)getTimerCurrentScaledCount(ALT_GPT_CPU_GLOBAL_TMR, SECS_TO_MILLISECS);
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief    Enables global interrupt
+
+This function enables/disables global interrupts.
+
+\param  fEnable_p               TRUE = enable interrupts
+                                FALSE = disable interrupts
+
+\ingroup module_target
+*/
+//------------------------------------------------------------------------------
+void target_enableGlobalInterrupt(UINT8 fEnable_p)
+{
+    static INT    lockCount = 0;
+
+    if (fEnable_p != FALSE) // restore interrupts
+    {
+        if (--lockCount == 0)
+        {
+            enableInterruptMaster();
+        }
+    }
+    else
+    {                       // disable interrupts
+        if (lockCount == 0)
+        {
+            disableInterruptMaster();
+        }
+
+        lockCount++;
+    }
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Initialize target specific stuff
+
+The function initialize target specific stuff which is needed to run the
+openPOWERLINK stack.
+
+\return The function returns a tOplkError error code.
+
+\ingroup module_target
+*/
+//------------------------------------------------------------------------------
+tOplkError target_init(void)
+{
+    tOplkError          oplkRet = kErrorOk;
+    ALT_STATUS_CODE     halRet = ALT_E_SUCCESS;
+
+#ifdef ALTARM_CACHE_ENABLE
+    // Enable Cache
+    halRet = alt_cache_system_enable();
+#else
+    halRet = alt_cache_system_disable();
+#endif
+
+    if (halRet != ALT_E_SUCCESS)
+    {
+        oplkRet = kErrorGeneralError;
+        goto Exit;
+    }
+
+    // Initialize the global interrupt controller
+    halRet = alt_int_global_init();
+    if (halRet != ALT_E_SUCCESS)
+    {
+        DEBUG_LVL_ERROR_TRACE("global IRQ controller initialization Failed!!\n");
+        oplkRet = kErrorGeneralError;
+        goto Exit;
+    }
+
+    // Initialize the cpu interrupt interface
+    halRet = alt_int_cpu_init();
+    if (halRet != ALT_E_SUCCESS)
+    {
+        DEBUG_LVL_ERROR_TRACE("CPU IRQ interface initialization Failed!!\n");
+        oplkRet = kErrorGeneralError;
+        goto Exit;
+    }
+
+Exit:
+    return oplkRet;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Cleanup target specific stuff
+
+The function cleans-up target specific stuff.
+
+\return The function returns a tOplkError error code.
+
+\ingroup module_target
+*/
+//------------------------------------------------------------------------------
+tOplkError target_cleanup(void)
+{
+    ALT_STATUS_CODE     halRet = ALT_E_SUCCESS;
+
+    disableInterruptMaster();
+    alt_int_cpu_uninit();
+    alt_int_global_uninit();
+    halRet = alt_cache_system_disable();
+
+    return kErrorOk;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief Sleep for the specified number of milliseconds
+
+The function makes the calling thread sleep until the number of specified
+milliseconds have elapsed.
+
+\param  milliSeconds_p            Number of milliseconds to sleep
+
+\ingroup module_target
+*/
+//------------------------------------------------------------------------------
+void target_msleep(UINT32 milliSeconds_p)
+{
+    msleep(milliSeconds_p);
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Set IP address of specified Ethernet interface
+
+The function sets the IP address, subnetMask and MTU of an Ethernet
+interface.
+
+\param  ifName_p                Name of Ethernet interface.
+\param  ipAddress_p             IP address to set for interface.
+\param  subnetMask_p            Subnet mask to set for interface.
+\param  mtu_p                   MTU to set for interface.
+
+\return The function returns a tOplkError error code.
+
+\ingroup module_target
+*/
+//------------------------------------------------------------------------------
+tOplkError target_setIpAdrs(char* ifName_p, UINT32 ipAddress_p, UINT32 subnetMask_p,
+                            UINT16 mtu_p)
+{
+    UNUSED_PARAMETER(ifName_p);
+    UNUSED_PARAMETER(ipAddress_p);
+    UNUSED_PARAMETER(subnetMask_p);
+    UNUSED_PARAMETER(mtu_p);
+
+    //Note: The given parameters are ignored because the application must set
+    //      these settings to the used IP stack by itself!
+
+    return kErrorOk;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief  Set default gateway for Ethernet interface
+
+The function sets the default gateway of an Ethernet interface.
+
+\param  defaultGateway_p            Default gateway to set.
+
+\return The function returns a tOplkError error code.
+
+\ingroup module_target
+*/
+//------------------------------------------------------------------------------
+tOplkError target_setDefaultGateway(UINT32 defaultGateway_p)
+{
+    UNUSED_PARAMETER(defaultGateway_p);
+
+    //Note: The given parameters are ignored because the application must set
+    //      these settings to the used IP stack by itself!
+
+    return kErrorOk;
+}
+
+//============================================================================//
+//            P R I V A T E   F U N C T I O N S                               //
+//============================================================================//
+/// \name Private Functions
+/// \{
+
+//------------------------------------------------------------------------------
+/**
+\brief Enable the global interrupt master
+
+The function enables interrupt reception in the global and target processor
+interrupt interfaces.
+
+\return The function returns an integer
+\retval 0                   Success
+\retval -1                  Failure
+*/
+//------------------------------------------------------------------------------
+static inline INT enableInterruptMaster(void)
+{
+    ALT_STATUS_CODE     retStatus = ALT_E_SUCCESS;
+    INT                 ret = 0;
+
+    // Enable global interrupt master
+    retStatus = alt_int_global_enable();
+    if (retStatus != ALT_E_SUCCESS)
+    {
+        DEBUG_LVL_ERROR_TRACE("enabling global interrupt receiver failed\n");
+        ret = -1;
+        goto Exit;
+    }
+
+    // CPU interface global enable
+    retStatus = alt_int_cpu_enable();
+    if (retStatus != ALT_E_SUCCESS)
+    {
+        DEBUG_LVL_ERROR_TRACE("enabling cpu interrupt receiver failed\n");
+        ret = -1;
+        goto Exit;
+    }
+
+Exit:
+    return ret;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief Disable the global interrupt master
+
+The function disables interrupt reception in the global and target processor
+interrupt interfaces.
+
+\return The function returns an integer
+\retval 0                   Success
+\retval -1                  Failure
+*/
+//------------------------------------------------------------------------------
+static inline INT disableInterruptMaster(void)
+{
+    ALT_STATUS_CODE     retStatus = ALT_E_SUCCESS;
+    INT                 ret = 0;
+
+    // Disable all interrupts from the distributor
+    retStatus = alt_int_global_disable();
+    if (retStatus != ALT_E_SUCCESS)
+    {
+        ret = -1;
+        goto Exit;
+    }
+
+    // Reset the CPU interface
+    retStatus = alt_int_cpu_disable();
+    if (retStatus != ALT_E_SUCCESS)
+    {
+        ret = -1;
+        goto Exit;
+    }
+
+Exit:
+    return ret;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief Convert timer ticks into time units
+
+The function returns the timestamp of the provided timer in standard time units.
+
+\param  timerId_p               The ALT_GPT_TIMER_t enum Id of the timer used
+\param  scalingFactor_p         Ratio of provided time duration scale to seconds
+
+\return The function returns a unsigned 64 bit value.
+\retval Timestamp from the given timer in standard time unit provided.
+*/
+//------------------------------------------------------------------------------
+static inline UINT64 getTimerCurrentScaledCount(ALT_GPT_TIMER_t timerId_p,
+                                                UINT32   scalingFactor_p)
+{
+    UINT64          timeStamp_l = 0;
+    UINT64          timeStamp_h = 0;
+    UINT64          timeStamp = 0;
+    UINT64          scaledTime = 0;
+    ALT_CLK_t       clkSrc = ALT_CLK_UNKNOWN;
+    UINT32          preScaler = 0;
+    UINT32          freq = 1;
+
+    preScaler = alt_gpt_prescaler_get(timerId_p);
+    if (preScaler <= UINT8_MAX)
+    {
+        if (timerId_p == ALT_GPT_CPU_GLOBAL_TMR)    // Global Timer
+        {
+            alt_globaltmr_get((UINT32*)&timeStamp_h, (UINT32*)&timeStamp_l);
+            clkSrc = ALT_CLK_MPU_PERIPH;
+        }
+        else
+        {
+            scaledTime = 0;
+            goto Exit;
+        }
+
+        if (alt_clk_freq_get(clkSrc, &freq) == ALT_E_SUCCESS)
+        {
+            timeStamp_l *= (preScaler + 1);
+            timeStamp_h *= (preScaler + 1);
+            timeStamp_l *= scalingFactor_p;
+            timeStamp_h *= scalingFactor_p;
+            timeStamp = (UINT64)((((timeStamp_h << 32) & ~UINT32_MAX) |
+                                   timeStamp_l) / freq);
+            scaledTime = (timeStamp > UINT64_MAX) ? UINT64_MAX : (UINT64)timeStamp;
+        }
+    }
+
+Exit:
+    return scaledTime;
+}
+
+//------------------------------------------------------------------------------
+/**
+\brief Get maximum timestamp of the timer
+
+The function returns the maximum timestamp of the provided timer
+in standard time units.
+
+\param  timerId_p                The ALT_GPT_TIMER_t enum Id of the timer used
+\param  scalingFactor_p          Ratio of provided time duration scale to seconds
+
+\return The function returns a unsigned 64 bit value.
+\retval Maximum timestamp from the given timer in standard time unit provided.
+*/
+//------------------------------------------------------------------------------
+static inline UINT64 getTimerMaxScaledCount(ALT_GPT_TIMER_t timerId_p,
+                                            UINT32 scalingFactor_p)
+{
+    UINT64          maxScaledTime = 0;
+    UINT32          freq = 1;
+    UINT64          maxTimeStampLow = 0;
+    UINT64          maxTimeStampHigh = 0;
+    UINT64          maxTimeStamp = 0;
+    UINT32          preScaler = 0;
+    ALT_CLK_t       clkSrc;
+
+    preScaler = alt_gpt_prescaler_get(timerId_p);
+
+    if (timerId_p == ALT_GPT_CPU_GLOBAL_TMR)
+    {
+        clkSrc = ALT_CLK_MPU_PERIPH;
+        maxTimeStampLow = (UINT64)UINT32_MAX;
+        maxTimeStampHigh = (UINT64)UINT32_MAX;
+    }
+    else
+    {
+        goto Exit;
+    }
+
+    if (alt_clk_freq_get(clkSrc, &freq) == ALT_E_SUCCESS)
+    {
+        maxTimeStampLow *= (preScaler + 1);
+        maxTimeStampHigh *= (preScaler + 1);
+        maxTimeStampLow *= scalingFactor_p;
+        maxTimeStampHigh *= scalingFactor_p;
+        maxTimeStamp = (UINT64)((((maxTimeStampHigh << 32) & ~UINT32_MAX) |
+                                  maxTimeStampLow) / freq);
+
+        maxScaledTime = (maxTimeStamp > UINT64_MAX) ? UINT64_MAX : (UINT64) maxTimeStamp;
+    }
+
+Exit:
+    return maxScaledTime;
+}
+
+///\}
