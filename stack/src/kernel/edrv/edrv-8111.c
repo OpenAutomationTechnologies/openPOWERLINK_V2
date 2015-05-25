850ab00b7b7377590bb98f434eedb648ef172edb
Avinash Alfred <avinash.a@kalycito.com>
2015-05-25 14:27:36 +0530

    [FIX] NMT cycle error in chained station
    TxPoll requests are lost when the Tx packets are too close.

--- stack/src/kernel/edrv/edrv-8111.c
+++ stack/src/kernel/edrv/edrv-8111.c
@@ -865,8 +865,11 @@ tOplkError edrv_sendTxBuffer(tEdrvTxBuffer* pBuffer_p)
 
     wmb();
 
+	// 8168 fix: TxPoll requests are lost when the Tx packets are too close
+	while ((EDRV_REGB_READ(RW_REGB_TX_PRIO_POLL) & RW_REGB_TX_PRIO_POLL_HI_PRIO_SEND) != 0);
+	
     // Notify the MAC about the pending Tx
-    EDRV_REGB_WRITE(RW_REGB_TX_PRIO_POLL, RW_REGB_TX_PRIO_POLL_HI_PRIO_SEND);
+	EDRV_REGB_WRITE(RW_REGB_TX_PRIO_POLL, RW_REGB_TX_PRIO_POLL_HI_PRIO_SEND);
 
 Exit:
     return ret;
