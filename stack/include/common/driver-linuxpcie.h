8fb96510d8ece27f6af11fc5be44840c0347d621
John <chidrupaya.sr@kalycito.com>
2015-10-16 16:53:13 +0530

    [FEATURE] Add PCIe interface support on Linux
    - Add stack generic definitions for Linux PCIe interface.
    - Add support for Linux PCIe in dualprocshm library.
    - Add Linux PCIe interface driver for accessing PCP over
      dualprocshm interface. The PCIe interface module consists of
      a driver for the PCIe interface and a module to communicate
      to the PCP using dualprocshm.
    
    Change-Id: I2051bfc11eb4dd93c08b655612ca630b9c7e4e55
    Signed-off-by: John <chidrupaya.sr@kalycito.com>

--- stack/include/common/driver-linuxpcie.h
+++ stack/include/common/driver-linuxpcie.h
@@ -1,16 +1,16 @@
 /**
 ********************************************************************************
-\file   common/driver.h
+\file   common/driver-linuxpcie.h
 
-\brief  Header file for openPOWERLINK drivers
+\brief  Header file for openPOWERLINK PCIe driver interface for Linux kernel
 
 This file contains the necessary definitions for using the openPOWERLINK
-kernel driver modules.
+PCIe driver interface for Linux kernel.
 *******************************************************************************/
 
 /*------------------------------------------------------------------------------
 Copyright (c) 2014, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
-Copyright (c) 2013, SYSTEC electronic GmbH
+Copyright (c) 2015, Kalycito Infotech Private Limited
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -36,108 +36,44 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ------------------------------------------------------------------------------*/
 
-#ifndef _INC_common_driver_H_
-#define _INC_common_driver_H_
+#ifndef _INC_common_driver_linux_pcie_H_
+#define _INC_common_driver_linux_pcie_H_
 
 //------------------------------------------------------------------------------
 // includes
 //------------------------------------------------------------------------------
-#include <common/oplkinc.h>
-#include <common/dllcal.h>
 
 //------------------------------------------------------------------------------
 // const defines
 //------------------------------------------------------------------------------
+#define PLK_CLASS_NAME    "plk"
+#define PLK_DEV_NAME      "plk" // used for "/dev" and "/proc" entry
+#define PLK_DRV_NAME      "plk"
+#define PLK_DEV_FILE      "/dev/plk"
+#define PLK_IOC_MAGIC     '='
+
+//------------------------------------------------------------------------------
+//  Commands for <ioctl>
+//------------------------------------------------------------------------------
+#define PLK_CMD_CTRL_EXECUTE_CMD                _IOWR(PLK_IOC_MAGIC, 0, tCtrlCmd)
+#define PLK_CMD_CTRL_STORE_INITPARAM            _IOW (PLK_IOC_MAGIC, 1, tCtrlInitParam)
+#define PLK_CMD_CTRL_READ_INITPARAM             _IOR (PLK_IOC_MAGIC, 2, tCtrlInitParam)
+#define PLK_CMD_CTRL_GET_STATUS                 _IOR (PLK_IOC_MAGIC, 3, UINT16)
+#define PLK_CMD_CTRL_GET_HEARTBEAT              _IOR (PLK_IOC_MAGIC, 4, UINT16)
+#define PLK_CMD_POST_EVENT                      _IOW (PLK_IOC_MAGIC, 5, tEvent)
+#define PLK_CMD_GET_EVENT                       _IOR (PLK_IOC_MAGIC, 6, tEvent)
+#define PLK_CMD_DLLCAL_ASYNCSEND                _IO  (PLK_IOC_MAGIC, 7)
+#define PLK_CMD_ERRHND_WRITE                    _IOW (PLK_IOC_MAGIC, 8, tErrHndIoctl)
+#define PLK_CMD_ERRHND_READ                     _IOR (PLK_IOC_MAGIC, 9, tErrHndIoctl)
+#define PLK_CMD_TIMESYNC_SYNC                   _IO  (PLK_IOC_MAGIC, 10)
+#define PLK_CMD_PDO_MAP_OFFSET                  _IOR (PLK_IOC_MAGIC, 11, ULONG)
 
 //------------------------------------------------------------------------------
 // typedef
 //------------------------------------------------------------------------------
-typedef struct
-{
-    tDllCalQueue            queue;
-    size_t                  size;
-    void*                   pData;
-} tIoctlDllCalAsync;
-
-typedef struct
-{
-    void*                   pData;
-    size_t                  size;
-} tIoctlBufInfo;
-
-typedef struct
-{
-    UINT32                  offset;
-    UINT32                  errVal;
-} tErrHndIoctl;
-
-/**
-\brief IOCTL file chunk structure
-
-The structure is used to forward a file chunk to the kernel stack. The descriptor
-holds information about the current file chunk and the transfer progress.
-*/
-typedef struct
-{
-    tOplkApiFileChunkDesc   desc;       ///< File chunk descriptor
-    void*                   pData;      ///< Pointer to file chunk
-} tIoctlFileChunk;
-
-/**
-\brief PDO memory structure
-
-The structure is used to retrieve the PDO memory allocated by openPOWERLINK
-kernel stack and mapped into user virtual address space.
-*/
-typedef struct
-{
-    UINT32                  memSize;        ///< Size of PDO to be allocated and mapped
-    UINT32                  pdoMemOffset;   ///< Offset of PDO memory returned by kernel
-} tPdoMem;
-
-/**
-\brief Benchmark memory structure
-
-The structure is used to retrieve the benchmark port(PIO in FPGA) memory
-in the kernel stack. The memory is mapped by the driver in the user virtual
-address space before sharing.
-*/
-typedef struct
-{
-    void*                   pBaseAddr;      ///< Pointer to the benchmark address returned by kernel
-} tBenchmarkMem;
-
-/**
-\brief Memory parameters for a mapped memory
-
-The structure contains parameters used to map openPOWERLINK kernel layer
-memory into user layer.
-*/
-typedef struct
-{
-    void*                   pKernelAddr;    ///< Pointer to the Kernel address
-    void*                   pUserAddr;      ///< Pointer to the User address
-    UINT32                  size;           ///< Size of the shared memory
-} tMemStruc;
 
 //------------------------------------------------------------------------------
 // function prototypes
 //------------------------------------------------------------------------------
 
-//------------------------------------------------------------------------------
-// include architecture specific definitions
-//------------------------------------------------------------------------------
-#if (TARGET_SYSTEM == _LINUX_)
-
-#ifdef __LINUX_PCIE__
-#include <common/driver-linuxpcie.h>
-#else
-
-#include <common/driver-linux.h>
-#endif
-
-#elif (TARGET_SYSTEM == _WIN32_)
-#include <common/driver-windows.h>
-#endif
-
-#endif /* _INC_common_driver_H_ */
+#endif /* _INC_common_driver_linux_pcie_H_ */
