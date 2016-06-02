f7b7ca563e1b5a34229af267e246da554bf2ed30
Wolfgang Seiss <wolfgang.seiss@br-automation.com>
2016-06-02 15:57:03 +0200

    [TASK] Use a locally administered MAC address in embedded demos
    FPGA targets using openMAC do not have a globally assigned MAC
    address in the demos. Therefore, a locally administered MAC address
    is used.
    
    Change-Id: Iad603830e3cf7d3ffce77f22b95b20a361ef2dbe

--- apps/demo_mn_embedded/src/main.c
+++ apps/demo_mn_embedded/src/main.c
@@ -11,7 +11,7 @@ application.
 *******************************************************************************/
 
 /*------------------------------------------------------------------------------
-Copyright (c) 2015, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+Copyright (c) 2016, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
 Copyright (c) 2013, SYSTEC electronic GmbH
 Copyright (c) 2015, Kalycito Infotech Private Ltd.All rights reserved.
 All rights reserved.
@@ -69,7 +69,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define IP_ADDR             0xc0a86401              // 192.168.100.1
 #define SUBNET_MASK         0xFFFFFF00              // 255.255.255.0
 #define DEFAULT_GATEWAY     0xC0A864FE              // 192.168.100.C_ADR_RT1_DEF_NODE_ID
-#define MAC_ADDR            0x00, 0x12, 0x34, 0x56, 0x78, NODEID
+#define MAC_ADDR            0x02, 0x88, 0xAB, 0x00, 0x00, NODEID    // Locally administered MAC address
 
 //------------------------------------------------------------------------------
 // module global vars
