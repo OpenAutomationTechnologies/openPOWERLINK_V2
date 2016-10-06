f86668d9e4d39eabaa0089269326963dd8f26026
Varun Rajendran <varun.ar@kalycito.com>
2016-10-06 14:56:05 +0530

    [FEATURE] Move Zynq PCAP design from ISE to Vivado toolchain
    - Rename Xilinx arm linux cross compiler CMake file.
    - Add Vivado compilers for cross compilation.
    - Remove ISE compilers.
    
    Change-Id: Ic97fd77eea60f95ceac781935be81575bb982388

--- cmake/toolchain-xilinx-vivado-arm-linux-eabi-gnu.cmake
+++ cmake/toolchain-xilinx-vivado-arm-linux-eabi-gnu.cmake
@@ -1,8 +1,8 @@
 ################################################################################
 #
-# CMake target configuration file for Xilinx ARM Linux Eabi
+# CMake target configuration file for Xilinx ARM Linux Eabihf
 #
-# Copyright (c) 2014, Kalycito Infotech Pvt. Ltd.
+# Copyright (c) 2016, Kalycito Infotech Pvt. Ltd.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -28,11 +28,11 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ################################################################################
 
-SET(XILINX_ISE_DIR /opt/Xilinx/14.7/ISE_DS)
+SET(XILINX_SDK_DIR /opt/Xilinx/)
 
 ################################################################################
 # Name of the target platform
-SET(CMAKE_SYSTEM Xilinx-ARM-linux-eabi)
+SET(CMAKE_SYSTEM ARM-linux-eabihf)
 SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_PROCESSOR arm)
 
@@ -40,11 +40,11 @@ SET(CMAKE_SYSTEM_PROCESSOR arm)
 SET(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER arm-xilinx-linux-gnueabi-gcc)
-SET(CMAKE_CXX_COMPILER arm-xilinx-linux-gnueabi-g++)
-SET(CMAKE_ASM-ATT_COMPILER arm-xilinx-linux-gnueabi-as)
+SET(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+SET(CMAKE_ASM-ATT_COMPILER arm-linux-gnueabihf-as)
 
-SET(CMAKE_FIND_ROOT_PATH ${XILINX_ISE_DIR}/EDK/gnu/arm/lin/arm-xilinx-linux-gnueabi/libc)
+SET(CMAKE_FIND_ROOT_PATH ${XILINX_SDK_DIR}/SDK/2016.2/gnu/aarch32/lin/gcc-arm-linux-gnueabi/arm-linux-gnueabihf/libc)
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries in the target and build host directories
@@ -54,4 +54,4 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 # cross compiler directives
 SET(MAKE_KERNEL_ARCH arm)
-SET(MAKE_KERNEL_CROSS_COMPILE ${XILINX_ISE_DIR}/EDK/gnu/arm/lin/bin/arm-xilinx-linux-gnueabi-)
+SET(MAKE_KERNEL_CROSS_COMPILE ${XILINX_SDK_DIR}/SDK/2016.2/gnu/aarch32/lin/gcc-arm-linux-gnueabi/bin/arm-linux-gnueabihf-)
