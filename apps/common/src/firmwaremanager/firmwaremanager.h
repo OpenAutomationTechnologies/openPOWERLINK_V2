73e373a04e3143cfbf7a02b3b11ae613145173dc
Joerg Zelenka <joerg.zelenka@br-automation.com>
2017-04-14 09:30:00 +0200

    [FEATURE] Add general header file for firmware manager
    Change-Id: I522aaffef15b908a71e73e2b2a6e49c8ddc9ec0d

---
+++ apps/common/src/firmwaremanager/firmwaremanager.h
@@ -0,0 +1,77 @@
+/**
+********************************************************************************
+\file   firmwaremanager.h
+
+\brief  Header file for the firmware manager modules
+
+This header file contains the general definitions for all firmware manager
+modules.
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+Copyright (c) 2017, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
+#ifndef _INC_firmwaremanager_H_
+#define _INC_firmwaremanager_H_
+
+//------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
+#include <oplk/oplk.h>
+
+//------------------------------------------------------------------------------
+// const defines
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// typedef
+//------------------------------------------------------------------------------
+
+/**
+\brief Enum with return values used by the firmware manager modules
+*/
+typedef enum
+{
+    kFwReturnOk = 0,                ///< Function call was successfull
+    kFwReturnInvalidParameter,      ///< An invalid parameter was passed
+    kFwReturnInvalidInstance,       ///< An invalid instance was passed
+    kFwReturnNoRessource,           ///< The allocation of required ressources failed
+} tFirmwareRet;
+
+//------------------------------------------------------------------------------
+// function prototypes
+//------------------------------------------------------------------------------
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _INC_firmwaremanager_H_ */
