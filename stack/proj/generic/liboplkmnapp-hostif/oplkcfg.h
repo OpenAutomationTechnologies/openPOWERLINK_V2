741c11f0b41373061d566a192be44506620bea55
Joerg Zelenka <joerg.zelenka@br-automation.com>
2017-05-31 10:57:41 +0200

    [FEATURE] Enable SoC time forward in host interface stack libraries
    Change-Id: I44ab2940c5ec6d47e0516fb60bd00fffa182d8e4

--- stack/proj/generic/liboplkmnapp-hostif/oplkcfg.h
+++ stack/proj/generic/liboplkmnapp-hostif/oplkcfg.h
@@ -63,6 +63,7 @@ The generic defines are valid for the whole openPOWERLINK stack.
 #define CONFIG_INCLUDE_CFM
 #define CONFIG_INCLUDE_VETH
 #define CONFIG_INCLUDE_IP
+#define CONFIG_INCLUDE_SOC_TIME_FORWARD
 
 #ifndef BENCHMARK_MODULES
 #define BENCHMARK_MODULES                   (0 |\
