a5b551d2ae5d9f901314c0fb1ff4e2d74b0cc652
Pavithra K <pavithra.k@kalycito.com>
2017-09-21 19:32:04 +0530

    [TASK] Improve code quality in ctrlucal modules
    Modify the parameter type of ctrlucal_getMappedMem() function from
    UINT32 to size_t for offset and memory size, UINT8** to void** for
    generic pointers.
    
    Change-Id: I1a08ec173a02648ae821d300d3c55fe42b85b858

--- stack/src/user/ctrl/ctrlucal-hostif.c
+++ stack/src/user/ctrl/ctrlucal-hostif.c
@@ -12,6 +12,7 @@ a shared memory block for communication with the kernel layer.
 
 /*------------------------------------------------------------------------------
 Copyright (c) 2016, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
+Copyright (c) 2017, Kalycito Infotech Private Limited
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -531,9 +532,9 @@ the provided offset and returns the address back.
 \ingroup module_ctrlucal
 */
 //------------------------------------------------------------------------------
-tOplkError ctrlucal_getMappedMem(UINT32 kernelOffs_p,
-                                 UINT32 size_p,
-                                 UINT8** ppUserMem_p)
+tOplkError ctrlucal_getMappedMem(size_t kernelOffs_p,
+                                 size_t size_p,
+                                 void** ppUserMem_p)
 {
     UNUSED_PARAMETER(kernelOffs_p);
     UNUSED_PARAMETER(size_p);
