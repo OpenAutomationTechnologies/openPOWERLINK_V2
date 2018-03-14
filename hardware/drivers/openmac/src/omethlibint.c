29ab11149be7e3bdbd9a70d5a60504631a36799f
Sebastian Huber <sebastian.huber@embedded-brains.de>
2018-03-14 14:20:19 +0100

    [TASK] ometh: Use r/w functions for descriptors
    Change-Id: Ibb6eeaadff2de0fc4decb4ee93135068b95441ac
    See: #0336

--- hardware/drivers/openmac/src/omethlibint.c
+++ hardware/drivers/openmac/src/omethlibint.c
@@ -128,20 +128,20 @@ void            omethRxIrqHandler
     pInfo = hEth->pRxNext;    // access to next rx info structure
     pDesc = pInfo->pDesc;    // access to rx descriptor
 
-    if(pDesc->flags.byte.high & FLAGS1_OWNER) return;    // leave IRQ if no rx-buffer available
+    if(ometh_rd_8(&pDesc->flags.byte.high) & FLAGS1_OWNER) return;    // leave IRQ if no rx-buffer available
 
     // !! here hHook is abused to save stack-variables (until required for its original reason)
     //    (faster processing)
     hHook = (OMETH_HOOK_H)&hEth->pRegBase->rxStatus;
     ometh_wr_16(&((ometh_status_typ*)hHook)->clrBit, OMETH_REG_IQUIT);
 
-    flags = pDesc->flags.word;
+    flags = ometh_rd_16(&pDesc->flags.word);
 
     // access to buffer structure and set packet length
-    pRxBuf = GET_TYPE_BASE( ometh_buf_typ , packet.data, pDesc->pData);
+    pRxBuf = GET_TYPE_BASE( ometh_buf_typ , packet.data, ometh_rd_32(&pDesc->pData));
 
-    pRxBuf->packet.length = (uint32_t)pDesc->len - 4;       // length without checksum
-    pRxBuf->timeStamp     = pDesc->time;                    // overtake timestamp to packet header
+    pRxBuf->packet.length = (uint32_t)ometh_rd_16(&pDesc->len) - 4;       // length without checksum
+    pRxBuf->timeStamp     = ometh_rd_32(&pDesc->time);                    // overtake timestamp to packet header
 
     if(ometh_rd_16(&((ometh_status_typ*)hHook)->value) & OMETH_REG_LOST)
     {
@@ -197,7 +197,7 @@ void            omethRxIrqHandler
                     if(hHook->pFct(hFilter->arg, &pRxBuf->packet, omethPacketFree) == 0)
                     {
                         // use new frame for next rx at this descriptor
-                        pDesc->pData = (uint32_t)&pQueue->pBuf->packet.data;
+                        ometh_wr_32(&pDesc->pData, (uint32_t)&pQueue->pBuf->packet.data);
 
                         pQueue->pBuf = 0;                    // remove buffer from list
 
@@ -227,8 +227,8 @@ void            omethRxIrqHandler
     }
 
     // pass buffer to MAC
-    pDesc->len                = hEth->rxLen;        // set maximum length for this buffer
-    pDesc->flags.byte.high    = pInfo->flags1;    // set owner and last flag
+    ometh_wr_16(&pDesc->len, hEth->rxLen);        // set maximum length for this buffer
+    ometh_wr_8(&pDesc->flags.byte.high, pInfo->flags1);    // set owner and last flag
     hEth->pRxNext            = pInfo->pNext;        // switch to next info for next rx
 }
 
@@ -265,24 +265,24 @@ void            omethTxIrqHandler
     while(1)
     {
         // if it was not the last queue-descriptor .. it was an auto answer buffer
-        if(pDesc->flags.byte.high & FLAGS1_SENT)
+        if(ometh_rd_8(&pDesc->flags.byte.high) & FLAGS1_SENT)
         {
-            i = (uint32_t)pDesc->flags.word & 0x0F;         // collisions of this frame
+            i = (uint32_t)ometh_rd_16(&pDesc->flags.word) & 0x0F;         // collisions of this frame
             hEth->stat.txDone[i]++;                            // count transmits depending on occurred collisions
             hEth->stat.txCollision += i;                    // count collisions
 
             // call free function with ptr
             if(pInfo->pFctFree)
             {
-                pInfo->pFctFree( GET_TYPE_BASE(ometh_packet_typ, data, pDesc->pData), pInfo->fctFreeArg, pDesc->time );
+                pInfo->pFctFree( GET_TYPE_BASE(ometh_packet_typ, data, ometh_rd_32(&pDesc->pData)), pInfo->fctFreeArg, ometh_rd_32(&pDesc->time) );
             }
 
             hEth->pTxFree[0] = pInfo->pNext;    // switch free ptr to next info structure
 
             hEth->cntTxQueueOut++;
 
-            pDesc->flags.byte.high    = 0;                // clear sent-flag
-            pDesc->pData            = 0;                  // mark buffer as free
+            ometh_wr_8(&pDesc->flags.byte.high, 0);                // clear sent-flag
+            ometh_wr_32(&pDesc->pData, 0);                  // mark buffer as free
             break;
         }
 
@@ -293,24 +293,24 @@ void            omethTxIrqHandler
             pDesc    = pInfo->pDesc;        // access to tx descriptor
 
             // if it was not the last queue-descriptor .. it was an auto answer buffer
-            if(pDesc->flags.byte.high & FLAGS1_SENT)
+            if(ometh_rd_8(&pDesc->flags.byte.high) & FLAGS1_SENT)
             {
-                i = (uint32_t)pDesc->flags.word & 0x0F;         // collisions of this frame
+                i = (uint32_t)ometh_rd_16(&pDesc->flags.word) & 0x0F;         // collisions of this frame
                 hEth->stat.txDone[i]++;                         // count transmits depending on occurred collisions
                 hEth->stat.txCollision += i;                    // count collisions
 
                 // call free function with ptr
                 if(pInfo->pFctFree)
                 {
-                    pInfo->pFctFree( GET_TYPE_BASE(ometh_packet_typ, data, pDesc->pData), pInfo->fctFreeArg, pDesc->time );
+                    pInfo->pFctFree( GET_TYPE_BASE(ometh_packet_typ, data, ometh_rd_32(&pDesc->pData)), pInfo->fctFreeArg, ometh_rd_32(&pDesc->time) );
                 }
 
                 hEth->pTxFree[1] = pInfo->pNext;    // switch free ptr to next info structure
 
                 hEth->cntTxQueueOut++;
 
-                pDesc->flags.byte.high    = 0;                // clear sent-flag
-                pDesc->pData            = 0;                  // mark buffer as free
+                ometh_wr_8(&pDesc->flags.byte.high, 0);                // clear sent-flag
+                ometh_wr_32(&pDesc->pData, 0);                  // mark buffer as free
                 break;
             }
         }
@@ -340,20 +340,20 @@ void            omethTxIrqHandler
 
             pDesc = pInfo->pDesc;                    // access to descriptor
 
-            if(pDesc->flags.byte.high & FLAGS1_SENT) break;    // descriptor found, break loop
+            if(ometh_rd_8(&pDesc->flags.byte.high) & FLAGS1_SENT) break;    // descriptor found, break loop
 
             pInfo = pInfo->pNext;                    // switch to next tx info
         }
 
         pInfo->autoTxCount++;
 
-        i = (uint32_t)pDesc->flags.word & 0x0F;         // collisions of this frame
+        i = (uint32_t)ometh_rd_16(&pDesc->flags.word) & 0x0F;         // collisions of this frame
         hEth->stat.txDone[i]++;                            // count transmits depending on occurred collisions
         hEth->stat.txCollision += i;                    // count collisions
 
         // set owner flag for next auto tx
-        pDesc->txStart            = pInfo->delayTime;
-        pDesc->flags.byte.high    = FLAGS1_OWNER | FLAGS1_TX_DELAY;
+        ometh_wr_32(&pDesc->txStart, pInfo->delayTime);
+        ometh_wr_8(&pDesc->flags.byte.high, FLAGS1_OWNER | FLAGS1_TX_DELAY);
         break;
     }
 
