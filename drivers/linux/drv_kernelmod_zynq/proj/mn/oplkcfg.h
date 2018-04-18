0384ea5308ffaf2170e4b3f7c0ba0d59a9c3ad6e
bharghavi_intern <bharghavi_intern@kalycito.com>
2018-04-18 17:07:01 +0530

    [TASK] Update net device name as plk_veth for all platforms
     - This commit updates the net device name (PLK_VETH_NAME) as
       'plk_veth' to make it generic for all platforms.
    
    Change-Id: I7cafe036a33c1b9e61dffac883f4e8e148ef4e42

--- drivers/linux/drv_kernelmod_zynq/proj/mn/oplkcfg.h
+++ drivers/linux/drv_kernelmod_zynq/proj/mn/oplkcfg.h
@@ -13,7 +13,7 @@ as the equivalent configurations in user and kernel layers of the stack.
 /*------------------------------------------------------------------------------
 Copyright (c) 2012, SYSTEC electronik GmbH
 Copyright (c) 2014, Bernecker+Rainer Industrie-Elektronik Ges.m.b.H. (B&R)
-Copyright (c) 2017, Kalycito Infotech Private Limited
+Copyright (c) 2018, Kalycito Infotech Private Limited
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -66,10 +66,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define CONFIG_INCLUDE_SOC_TIME_FORWARD
 
 #define CONFIG_DLLCAL_QUEUE                         CIRCBUF_QUEUE
-
-#if defined(__LINUX_ZYNQ__)
-#define PLK_VETH_NAME                                   "plk_veth" // name of net device in Linux
-#endif
 
 //==============================================================================
 // Data Link Layer (DLL) specific defines
