91559c9d5305d19893156970fc6b43b822a6f1c3
Priyadharshini Prabhakaran <priyadharshini.p@kalycito.com>
2018-06-29 10:49:04 +0530

    [TASK] Add CN Zynq hybrid design
     - Reuse Zynq hybrid MN configuration files for CN configuration
    
    Change-Id: Iec0e830d40e1e9a98848944f03f72996e3e1ffe9

---
+++ hardware/boards/xilinx-z702/cn-dual-shmem-gpio/include/oplkcfg-board.h
@@ -0,0 +1,49 @@
+/**
+********************************************************************************
+\file   oplkcfg-board.h
+
+\brief  Board specific configuration file
+
+This header file configures the POWERLINK node.
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+Copyright (c) 2018, B&R Industrial Automation GmbH
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
+
+#ifndef _INC_oplkcfg_board_H_
+#define _INC_oplkcfg_board_H_
+
+// Size of kernel internal queue
+#define CONFIG_EVENT_SIZE_CIRCBUF_KERNEL_INTERNAL   16384
+
+// Set number of Rx buffers for openMAC
+#define CONFIG_EDRV_RX_BUFFERS                      32
+
+// Disable deferred Rx frame release
+#define CONFIG_DLL_DEFERRED_RXFRAME_RELEASE_ASYNC   FALSE
+
+#endif /* _INC_oplkcfg_board_H_ */
